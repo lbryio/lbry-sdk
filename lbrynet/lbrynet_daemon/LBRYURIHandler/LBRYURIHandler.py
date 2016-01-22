808fbed4f36872fdfaa92764ae10e28d821bf545
jackrobison <jack@robisonservice.com>
2016-01-22 16:46:35 -0500

    Don't open links while having a 0.0 balance

--- lbrynet/lbrynet_daemon/LBRYURIHandler/LBRYURIHandler.py
+++ lbrynet/lbrynet_daemon/LBRYURIHandler/LBRYURIHandler.py
@@ -16,41 +16,43 @@ def main(args):
     daemon = xmlrpclib.ServerProxy('http://localhost:7080/')
 
     try:
-        b = daemon.get_balance()
+        balance = daemon.get_balance()
         is_running = True
+        if len(args) > 1:
+            print 'Too many args', args
+
+        elif is_running:
+            if args[0][7:] == 'lbry':
+                daemon.render_gui()
+
+            elif args[0][7:] == 'settings':
+                r = daemon.get_settings()
+                html = "<body>" + json.dumps(r) + "</body>"
+                r = daemon.render_html(html)
+            else:
+                if float(balance) > 0.0:
+                    r = daemon.get(args[0][7:])
+                    print r
+                    path = r['path']
+                    if path[0] != '/':
+                        path = '/' + path
+
+                    print path
+                    filename = path.split('/')[len(path.split('/')) - 1]
+                    extension = path.split('.')[len(path.split('.')) - 1]
+
+                    if extension in ['mp4', 'flv', 'mov']:
+                        html = render_video(path)
+                        daemon.render_html(html)
+
+                    else:
+                        webbrowser.open('file://' + str(path))
+
     except:
         webbrowser.open('http://lbry.io/get')
         is_running = False
 
-    if len(args) > 1:
-        print 'Too many args', args
 
-    elif is_running:
-        if args[0][7:] == 'lbry':
-            daemon.render_gui()
-
-        elif args[0][7:] == 'settings':
-            r = daemon.get_settings()
-            html = "<body>" + json.dumps(r) + "</body>"
-            r = daemon.render_html(html)
-
-        else:
-            r = daemon.get(args[0][7:])
-            print r
-            path = r['path']
-            if path[0] != '/':
-                path = '/' + path
-
-            print path
-            filename = path.split('/')[len(path.split('/')) - 1]
-            extension = path.split('.')[len(path.split('.')) - 1]
-
-            if extension in ['mp4', 'flv', 'mov']:
-                html = render_video(path)
-                daemon.render_html(html)
-
-            else:
-                webbrowser.open('file://' + str(path))
 
 
 if __name__ == "__main__":
