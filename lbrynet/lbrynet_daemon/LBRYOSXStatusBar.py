59d08a92d00ec8e6c08419c078c139639c842d2c
Jack <jack@robisonservice.com>
2016-02-27 17:49:49 -0500

    update status bar app

--- lbrynet/lbrynet_daemon/LBRYOSXStatusBar.py
+++ lbrynet/lbrynet_daemon/LBRYOSXStatusBar.py
@@ -3,23 +3,69 @@ import xmlrpclib
 import os
 import webbrowser
 
+
 class DaemonStatusBarApp(rumps.App):
     def __init__(self):
-        super(DaemonStatusBarApp, self).__init__("LBRYnet", icon=os.path.join(os.path.expanduser("~"), "Downloads/lbryio//web/img/fav/apple-touch-icon.png"), quit_button=None)
-        self.menu = ["Open UI", "Quit"]
+        icon_path = os.path.join(os.path.expanduser("~"), "Downloads/lbryio/web/img/fav/apple-touch-icon.png")
+        if os.path.isfile(icon_path):
+            rumps.App.__init__(self, name="LBRY", icon=icon_path, quit_button=None,
+                                menu=["Open", "Preferences", "View balance", "Quit"])
+        else:
+            rumps.App.__init__(self, name="LBRY", title="LBRY", quit_button=None,
+                                menu=["Open", "Preferences", "View balance", "Quit"])
 
-    @rumps.clicked('Open UI')
+    @rumps.clicked('Open')
     def get_ui(self):
-        webbrowser.open("lbry://lbry")
+        try:
+            daemon = xmlrpclib.ServerProxy('http://localhost:7080')
+            daemon.is_running()
+            webbrowser.open("lbry://lbry")
+        except:
+            try:
+                rumps.notification(title='LBRY', subtitle='status', message="Couldn't connect to lbrynet daemon", sound=True)
+            except:
+                rumps.alert(title='LBRY', message="Couldn't connect to lbrynet daemon")
+
+    @rumps.clicked("Preferences")
+    def prefs(self, _):
+        try:
+            daemon = xmlrpclib.ServerProxy('http://localhost:7080')
+            daemon.is_running()
+            webbrowser.open("lbry://settings")
+        except:
+            rumps.notification(title='LBRY', subtitle='status', message="Couldn't connect to lbrynet daemon", sound=True)
+
+    @rumps.clicked("View balance")
+    def disp_balance(self):
+        daemon = xmlrpclib.ServerProxy('http://localhost:7080')
+        try:
+            balance = daemon.get_balance()
+            try:
+                rumps.notification(title='LBRY', subtitle='status', message="Your balance is " + str(balance), sound=False)
+            except:
+                rumps.alert(title='LBRY', message="Your balance is " + str(balance))
+
+        except:
+            try:
+                rumps.notification(title='LBRY', subtitle='status', message="Couldn't connect to lbrynet daemon", sound=True)
+            except:
+                rumps.alert(title='LBRY', message="Couldn't connect to lbrynet daemon")
 
     @rumps.clicked('Quit')
     def clean_quit(self):
         daemon = xmlrpclib.ServerProxy('http://localhost:7080')
-        daemon.stop()
+        try:
+            daemon.stop()
+        except:
+            pass
+
         rumps.quit_application()
 
+
 def main():
-    DaemonStatusBarApp().run()
+    status_app = DaemonStatusBarApp()
+    status_app.run()
+
 
 if __name__ == '__main__':
     main()
