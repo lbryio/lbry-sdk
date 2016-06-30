62b80c13cc870372e06514463a02c190973d2af5
Jack <jackrobison@lbry.io>
2016-06-30 01:26:33 -0400

    version bump

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -4,5 +4,5 @@ log = logging.getLogger(__name__)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 log.setLevel(logging.ERROR)
 
-version = (0, 2, 6)
+version = (0, 3, 0)
 __version__ = ".".join([str(x) for x in version])
