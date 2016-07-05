8336930091c38db9ace8af67d3d6a10cfad53dbb
Alex Grintsvayg <alex@grin.io>
2016-07-05 15:20:35 -0400

    Bump version: 0.3.2 → 0.3.3

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -4,5 +4,5 @@ log = logging.getLogger(__name__)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 log.setLevel(logging.ERROR)
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 version = tuple(__version__.split('.'))
