5221631b40c8fe0fa93d9ca53c7863add5422b7f
Jack <jack@lbry.io>
2016-07-12 17:30:58 -0400

    Bump version: 0.3.5 -> 0.3.6

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -4,5 +4,5 @@ log = logging.getLogger(__name__)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 log.setLevel(logging.ERROR)
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 version = tuple(__version__.split('.'))
