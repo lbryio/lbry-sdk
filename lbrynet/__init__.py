5ebcc01bcca76b62999bbc0133fdd62b98176123
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-07-17 14:10:11 -0500

    Bump version: 0.3.6 -> 0.3.7

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -4,5 +4,5 @@ log = logging.getLogger(__name__)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 log.setLevel(logging.ERROR)
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 version = tuple(__version__.split('.'))
