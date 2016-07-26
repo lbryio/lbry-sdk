ca45f461ae7b11fddb678096067d022ae88ee910
Jack <jack@lbry.io>
2016-07-25 23:42:03 -0400

    spelling

--- lbrynet/core/LBRYMetadata.py
+++ lbrynet/core/LBRYMetadata.py
@@ -17,7 +17,7 @@ OPTIONAL_METADATA_FIELDS = ['thumbnail', 'preview', 'fee', 'contact', 'pubkey']
 #v0.0.1 metadata
 METADATA_REVISIONS = {'0.0.1': {'required': BASE_METADATA_FIELDS, 'optional': OPTIONAL_METADATA_FIELDS}}
 #v0.0.2 metadata additions
-METADATA_REVISIONS['0.0.2'] = {'required': ['nsfw', 'ver'], 'optional': ['licence_url']}
+METADATA_REVISIONS['0.0.2'] = {'required': ['nsfw', 'ver'], 'optional': ['license_url']}
 CURRENT_METADATA_VERSION = '0.0.2'
 
 
