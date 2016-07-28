4223298634ba1e5900ce600e622e30d7141b8331
Jack <jack@lbry.io>
2016-07-28 02:49:35 -0400

    test wrong metadata version

--- tests/lbrynet/core/test_LBRYMetadata.py
+++ tests/lbrynet/core/test_LBRYMetadata.py
@@ -127,7 +127,23 @@ class MetadataTest(unittest.TestCase):
         m = LBRYMetadata.Metadata(metadata)
         self.assertEquals('0.0.2', m.meta_version)
 
-
+    def test_version_claimed_is_001_but_version_is_002(self):
+        metadata = {
+            'license': 'NASA',
+            'fee': {'USD': {'amount': 0.01, 'address': 'baBYSK7CqGSn5KrEmNmmQwAhBSFgo6v47z'}},
+            'ver': '0.0.1',
+            'description': 'SDO captures images of the sun in 10 different wavelengths, each of which helps highlight a different temperature of solar material. Different temperatures can, in turn, show specific structures on the sun such as solar flares, which are gigantic explosions of light and x-rays, or coronal loops, which are stream of solar material travelling up and down looping magnetic field lines',
+            'language': 'en',
+            'author': 'The SDO Team, Genna Duberstein and Scott Wiessinger',
+            'title': 'Thermonuclear Art',
+            'sources': {
+                'lbry_sd_hash': '8655f713819344980a9a0d67b198344e2c462c90f813e86f0c63789ab0868031f25c54d0bb31af6658e997e2041806eb'},
+            'nsfw': False,
+            'content-type': 'video/mp4',
+            'thumbnail': 'https://svs.gsfc.nasa.gov/vis/a010000/a012000/a012034/Combined.00_08_16_17.Still004.jpg'
+        }
+        with self.assertRaises(AssertionError):
+            LBRYMetadata.Metadata(metadata)
 
 
 
