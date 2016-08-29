b0b222afa80a8ec242f83c1df97d8dd369ed08a1
Jack <jack@lbry.io>
2016-08-29 14:55:26 -0400

    add errback

--- tests/functional/test_reflector.py
+++ tests/functional/test_reflector.py
@@ -49,6 +49,7 @@ class TestReflector(unittest.TestCase):
                 raise unittest.SkipTest("TODO: fix this for windows")
 
         d.addCallback(lambda _: threads.deferToThread(delete_test_env))
+        d.addErrback(lambda err: str(err))
         return d
 
     def test_reflector(self):
