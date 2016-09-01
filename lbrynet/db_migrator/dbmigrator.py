aaf59218dab11ddcb6a016dc2d6002fe4f355b6e
Jack <jack@lbry.io>
2016-08-31 20:41:29 -0400

    comment out migrate0to1 import

--- lbrynet/db_migrator/dbmigrator.py
+++ lbrynet/db_migrator/dbmigrator.py
@@ -5,13 +5,13 @@ import os
 def migrate_db(db_dir, start, end):
     current = start
     old_dirs = []
-    if os.name == "nt":
-        return old_dirs
-    while current < end:
-        if current == 0:
-            from lbrynet.db_migrator.migrate0to1 import do_migration
-            old_dirs.append(do_migration(db_dir))
-            current += 1
+    # if os.name == "nt":
+    #     return old_dirs
+    # while current < end:
+    #     if current == 0:
+    #         from lbrynet.db_migrator.migrate0to1 import do_migration
+    #         old_dirs.append(do_migration(db_dir))
+    #         current += 1
     return old_dirs
 
 
