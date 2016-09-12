f352a3c597571b654c9e0e608c306265ed846584
Jack <jack@lbry.io>
2016-09-12 13:16:03 -0400

    rename abandon_name to abandon_claim

--- lbrynet/lbrynet_daemon/LBRYDaemon.py
+++ lbrynet/lbrynet_daemon/LBRYDaemon.py
@@ -2018,7 +2018,7 @@ class LBRYDaemon(jsonrpc.JSONRPC):
 
         return d
 
-    def jsonrpc_abandon_name(self, p):
+    def jsonrpc_abandon_claim(self, p):
         """
         Abandon a name and reclaim credits from the claim
 
