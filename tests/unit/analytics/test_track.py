813267ae18fec3d80a5d96041addb6d041961f39
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-10-11 15:12:10 -0500

    fix tests

--- tests/unit/analytics/test_track.py
+++ tests/unit/analytics/test_track.py
@@ -4,17 +4,17 @@ from twisted.trial import unittest
 
 
 class TrackTest(unittest.TestCase):
-    def test_empty_summarize_is_zero(self):
+    def test_empty_summarize_is_None(self):
         track = analytics.Track()
-        result = track.summarize('a')
-        self.assertEqual(0, result)
+        _, result = track.summarize('a')
+        self.assertEqual(None, result)
 
     def test_can_get_sum_of_metric(self):
         track = analytics.Track()
         track.add_observation('b', 1)
         track.add_observation('b', 2)
 
-        result = track.summarize('b')
+        _, result = track.summarize('b')
         self.assertEqual(3, result)
 
     def test_summarize_resets_metric(self):
@@ -23,5 +23,5 @@ class TrackTest(unittest.TestCase):
         track.add_observation('metric', 2)
 
         track.summarize('metric')
-        result = track.summarize('metric')
-        self.assertEqual(0, result)
+        _, result = track.summarize('metric')
+        self.assertEqual(None, result)
