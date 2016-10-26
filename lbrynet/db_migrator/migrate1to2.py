0028c9cdbe1f97ef712aa4832015741ca67bd6c3
kkurokawa <umpeikurokawa@gmail.com>
2016-10-26 10:49:37 -0400

    changing -1 to constant UNSET_NOUT

--- lbrynet/db_migrator/migrate1to2.py
+++ lbrynet/db_migrator/migrate1to2.py
@@ -3,7 +3,7 @@ import os
 import logging
 
 log = logging.getLogger(__name__)
-
+UNSET_NOUT = -1 
 
 def do_migration(db_dir):
     log.info("Doing the migration")
@@ -36,12 +36,12 @@ def migrate_blockchainname_db(db_dir):
     name_metadata = file_cursor.execute("select * from name_metadata").fetchall()
     claim_metadata = file_cursor.execute("select * from claim_ids").fetchall()
 
-    # fill n as -1, Wallet.py will be responsible for filling in correct n 
+    # fill n as V1_UNSET_NOUT, Wallet.py will be responsible for filling in correct n 
     for name, txid, sd_hash in name_metadata:
-        mem_cursor.execute("insert into name_metadata values (?, ?, ?, ?) ", (name, txid, -1, sd_hash))
+        mem_cursor.execute("insert into name_metadata values (?, ?, ?, ?) ", (name, txid, UNSET_NOUT, sd_hash))
 
     for claim_id, name, txid in claim_metadata:
-        mem_cursor.execute("insert into claim_ids values (?, ?, ?, ?)", (claim_id, name, txid, -1))
+        mem_cursor.execute("insert into claim_ids values (?, ?, ?, ?)", (claim_id, name, txid, UNSET_NOUT))
     temp_db.commit()
 
     new_name_metadata = mem_cursor.execute("select * from name_metadata").fetchall()
