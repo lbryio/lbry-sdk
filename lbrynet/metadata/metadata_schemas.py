fab2ca4a1221cf594d58e527d57dea88e371793b
Alex Liebowitz <alex@alexliebowitz.com>
2016-10-28 12:00:37 -0400

    Metadata version 0.0.4 with new "channel" field
    Will store a fully resolved URI (name/txid) or some other unique identifier
    for the channel the content is associated with.

--- lbrynet/metadata/metadata_schemas.py
+++ lbrynet/metadata/metadata_schemas.py
@@ -267,3 +267,102 @@ VER_003 = {
         'sig': ['pubkey']
     }
 }
+
+VER_003 = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'title': 'LBRY metadata schema 0.0.3',
+    'definitions': {
+        'fee_info': {
+            'type': 'object',
+            'properties': {
+                'amount': {
+                    'type': 'number',
+                    'minimum': 0,
+                    'exclusiveMinimum': True,
+                },
+                'address': {
+                    'type': 'string'
+                }
+            },
+        }
+    },
+    'type': 'object',
+
+    'properties': {
+        'ver': {
+            'type': 'string',
+            'enum': ['0.0.3'],
+        },
+        'title': {
+            'type': 'string'
+        },
+        'description': {
+            'type': 'string'
+        },
+        'author': {
+            'type': 'string'
+        },
+        'language': {
+            'type': 'string'
+        },
+        'license': {
+            'type': 'string'
+        },
+        'content_type': {
+            'type': 'string'
+        },
+        'sources': {
+            'type': 'object',
+            'properties': {
+                'lbry_sd_hash': {
+                    'type': 'string'
+                },
+                'btih': {
+                    'type': 'string'
+                },
+                'url': {
+                    'type': 'string'
+                }
+            },
+            'additionalProperties': False
+        },
+        'thumbnail': {
+            'type': 'string'
+        },
+        'preview': {
+            'type': 'string'
+        },
+        'fee': {
+            'properties': {
+                'LBC': { '$ref': '#/definitions/fee_info' },
+                'BTC': { '$ref': '#/definitions/fee_info' },
+                'USD': { '$ref': '#/definitions/fee_info' }
+            }
+        },
+        'contact': {
+            'type': 'number'
+        },
+        'pubkey': {
+            'type': 'string'
+        },
+        'channel': {
+            'type': 'string'
+        },
+        'license_url': {
+            'type': 'string'
+        },
+        'nsfw': {
+            'type': 'boolean',
+            'default': False
+        },
+        'sig': {
+            'type': 'string'
+        }
+    },
+    'required': ['ver', 'title', 'description', 'author',  'language', 'license', 'content_type', 'sources', 'nsfw'],
+    'additionalProperties': False,
+    'dependencies': {
+        'pubkey': ['sig'],
+        'sig': ['pubkey']
+    }
+}
