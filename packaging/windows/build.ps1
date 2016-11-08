aec7add402525d67e3317cbbb87991df04b2cd2d
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-11-08 14:01:06 -0600

    fixup! trying some powershell. wish me luck

---
+++ packaging/windows/build.ps1
@@ -0,0 +1,14 @@
+
+# this is a port of setup_qa.sh used for the unix platforms
+If ($(APPVEYOR_REPO_TAG) -Match "true") {
+   SHA = $(APPVEYOR_REPO_COMMIT).substring(0,10)
+   cat lbrynet\__init__.py | % { $_ -replace '__version__ = ".*"','__version__ = "$(SHA)"' } > newfile
+   Move-Item newfile lbrynet\__init__.py
+
+   wget https://s3.amazonaws.com/lbry-ui/development/dist.zip -OutFile dist.zip
+   Expand-Archive dist.zip -dest lbrynet\resources\ui
+   wget https://s3.amazonaws.com/lbry-ui/development/data.json -OutFile lbrynet\resources\ui\data.json
+}
+
+C:\Python27\python.exe setup.py build bdist_msi
+signtool.exe sign /f packaging\windows\certs\lbry2.pfx /p %key_pass% /tr http://tsa.starfieldtech.com /td SHA256 /fd SHA256 dist\*.msi
