112ec280c3404c4e2beae1334e2387caf1353c65
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-11-08 14:22:07 -0600

    fixup! Bundle the UI into non-tagged builds.

--- packaging/travis/setup_qa.sh
+++ packaging/travis/setup_qa.sh
@@ -2,9 +2,9 @@
 
 # changes here also need to be added to build.ps1 for windows
 
-SHA=echo ${TRAVIS_COMMIT} | cut -c1-10
+SHA=$(echo ${TRAVIS_COMMIT} | cut -c1-10)
 sed -i "s/__version__ = \".*\"/__version__ = \"${SHA}\"/g" lbrynet/__init__.py
 
-wget https://s3.amazonaws.com/lbry-ui/development/dist.zip
-unzip dist.zip -d lbrynet/resources/ui
+wget https://s3.amazonaws.com/lbry-ui/development/dist.zip -O dist.zip
+unzip -oq dist.zip -d lbrynet/resources/ui
 wget https://s3.amazonaws.com/lbry-ui/development/data.json -O lbrynet/resources/ui/data.json
