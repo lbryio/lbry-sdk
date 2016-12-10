fec917b9c1ec221905c1a69961eedf63112f3999
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-12-10 12:01:29 -0800

    rename platform to system_info

--- lbrynet/analytics/manager.py
+++ lbrynet/analytics/manager.py
@@ -3,7 +3,7 @@ from lbrynet.core import looping_call_manager
 from twisted.internet import defer
 from twisted.internet import task
 
-from lbrynet.core.Platform import get_platform
+from lbrynet.core.system_info import get_platform
 from lbrynet.conf import settings
 
 import constants
