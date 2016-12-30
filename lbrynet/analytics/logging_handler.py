123fda1844d90d7cd501324dc7f8d0bc2640bfd0
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-12-30 14:31:43 -0600

    track errors in analytics

---
+++ lbrynet/analytics/logging_handler.py
@@ -0,0 +1,11 @@
+import logging
+
+
+class Handler(logging.Handler):
+    """A logging handler that reports errors to the analytics manager"""
+    def __init__(self, manager, level=logging.ERROR):
+        self.manager = manager
+        logging.Handler.__init__(self, level)
+
+    def emit(self, record):
+        self.manager.send_error(record.message)
