e95adecd41ae63fbf23d38250216f1eca43a0aca
Alex Liebowitz <alex@alexliebowitz.com>
2017-01-13 13:35:32 -0500

    Rename jsonrpc_open_file to jsonrpc_open

--- lbrynet/lbrynet_daemon/Daemon.py
+++ lbrynet/lbrynet_daemon/Daemon.py
@@ -2220,7 +2220,7 @@ class Daemon(AuthJSONRPCServer):
         return d
 
     @AuthJSONRPCServer.auth_required
-    def jsonrpc_open_file(self, p):
+    def jsonrpc_open(self, p):
         """
         Instruct the OS to open a file.
 
