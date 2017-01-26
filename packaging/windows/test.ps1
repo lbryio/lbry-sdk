24e7b7797cb9e0c5cc92ec1f9d44327b1ddb4fd7
jobevers <jobevers@users.noreply.github.com>
2017-01-26 10:00:45 -0600

    remove packaging

--- packaging/windows/test.ps1
+++
@@ -1,6 +0,0 @@
-C:\Python27\Scripts\pip.exe install mock
-C:\Python27\Scripts\pip.exe install pylint
-C:\Python27\python.exe C:\Python27\Scripts\trial.py C:\projects\lbry\tests\unit
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-C:\Python27\Scripts\pylint.exe -E --disable=inherit-non-class --disable=no-member --ignored-modules=distutils --enable=unused-import --enable=bad-whitespace --enable=line-too-long lbrynet packaging/windows/lbry-win32-app/LBRYWin32App.py
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
