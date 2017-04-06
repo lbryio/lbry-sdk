a2eb0cad33ef2830d884905781649118f879facf
Alex Grintsvayg <grin@lbry.io>
2017-04-06 19:56:36 -0400

    delete live lbrylive livestreaming code

--- tests/functional/test_misc.py
+++ tests/functional/test_misc.py
@@ -12,15 +12,9 @@ from Crypto.PublicKey import RSA
 from Crypto import Random
 from Crypto.Hash import MD5
 from lbrynet import conf
-from lbrynet.lbrylive.LiveStreamCreator import FileLiveStreamCreator
-from lbrynet.lbrylive.LiveStreamMetadataManager import DBLiveStreamMetadataManager
-from lbrynet.lbrylive.LiveStreamMetadataManager import TempLiveStreamMetadataManager
 from lbrynet.lbryfile.EncryptedFileMetadataManager import TempEncryptedFileMetadataManager, \
     DBEncryptedFileMetadataManager
 from lbrynet import analytics
-from lbrynet.lbrylive.LiveStreamCreator import FileLiveStreamCreator
-from lbrynet.lbrylive.LiveStreamMetadataManager import DBLiveStreamMetadataManager
-from lbrynet.lbrylive.LiveStreamMetadataManager import TempLiveStreamMetadataManager
 from lbrynet.lbryfile.EncryptedFileMetadataManager import TempEncryptedFileMetadataManager
 from lbrynet.lbryfile.EncryptedFileMetadataManager import DBEncryptedFileMetadataManager
 from lbrynet.lbryfilemanager.EncryptedFileManager import EncryptedFileManager
@@ -43,10 +37,6 @@ from lbrynet.core.PeerManager import PeerManager
 from lbrynet.core.RateLimiter import DummyRateLimiter, RateLimiter
 from lbrynet.core.server.BlobRequestHandler import BlobRequestHandlerFactory
 from lbrynet.core.server.ServerProtocol import ServerProtocolFactory
-
-from lbrynet.lbrylive.server.LiveBlobInfoQueryHandler import CryptBlobInfoQueryHandlerFactory
-from lbrynet.lbrylive.client.LiveStreamOptions import add_live_stream_to_sd_identifier
-from lbrynet.lbrylive.client.LiveStreamDownloader import add_full_live_stream_downloader_to_sd_identifier
 
 from tests import mocks
 
@@ -304,132 +294,6 @@ def start_lbry_reuploader(sd_hash, kill_event, dead_event,
 
     d = task.deferLater(reactor, 1.0, start_transfer)
     d.addCallback(lambda _: start_server())
-    if not reactor.running:
-        reactor.run()
-
-
-def start_live_server(sd_hash_queue, kill_event, dead_event):
-    use_epoll_on_linux()
-    from twisted.internet import reactor
-
-    logging.debug("In start_server.")
-
-    Random.atfork()
-
-    r = random.Random()
-    r.seed("start_live_server")
-
-    wallet = FakeWallet()
-    peer_manager = PeerManager()
-    peer_finder = FakePeerFinder(5553, peer_manager, 1)
-    hash_announcer = FakeAnnouncer()
-    rate_limiter = DummyRateLimiter()
-    sd_identifier = StreamDescriptorIdentifier()
-
-    db_dir = "server"
-    os.mkdir(db_dir)
-
-    session = Session(conf.ADJUSTABLE_SETTINGS['data_rate'][1], db_dir=db_dir, lbryid="abcd",
-                      peer_finder=peer_finder, hash_announcer=hash_announcer, peer_port=5553,
-                      use_upnp=False, rate_limiter=rate_limiter, wallet=wallet,
-                      blob_tracker_class=DummyBlobAvailabilityTracker,
-                      is_generous=conf.ADJUSTABLE_SETTINGS['is_generous_host'][1])
-    stream_info_manager = DBLiveStreamMetadataManager(session.db_dir, hash_announcer)
-
-    logging.debug("Created the session")
-
-    server_port = []
-
-    def start_listening():
-        logging.debug("Starting the server protocol")
-        query_handler_factories = {
-            1: CryptBlobInfoQueryHandlerFactory(stream_info_manager, session.wallet,
-                                             session.payment_rate_manager),
-            2: BlobRequestHandlerFactory(session.blob_manager, session.wallet,
-                                      session.payment_rate_manager,
-                                      analytics.Track()),
-            3: session.wallet.get_wallet_info_query_handler_factory()
-        }
-
-        server_factory = ServerProtocolFactory(session.rate_limiter,
-                                               query_handler_factories,
-                                               session.peer_manager)
-        server_port.append(reactor.listenTCP(5553, server_factory))
-        logging.debug("Server protocol has started")
-
-    def create_stream():
-        logging.debug("Making the live stream")
-        test_file = GenFile(5209343, b''.join([chr(i + 2) for i in xrange(0, 64, 6)]))
-        stream_creator_helper = FileLiveStreamCreator(session.blob_manager, stream_info_manager,
-                                                      "test_file", test_file)
-        d = stream_creator_helper.setup()
-        d.addCallback(lambda _: stream_creator_helper.publish_stream_descriptor())
-        d.addCallback(put_sd_hash_on_queue)
-        d.addCallback(lambda _: stream_creator_helper.start_streaming())
-        return d
-
-    def put_sd_hash_on_queue(sd_hash):
-        logging.debug("Telling the client to start running. Stream hash: %s", str(sd_hash))
-        sd_hash_queue.put(sd_hash)
-        logging.debug("sd hash has been added to the queue")
-
-    def set_dead_event():
-        logging.debug("Setting the dead event")
-        dead_event.set()
-
-    def print_error(err):
-        logging.debug("An error occurred during shutdown: %s", err.getTraceback())
-
-    def stop_reactor():
-        logging.debug("Server is stopping its reactor")
-        reactor.stop()
-
-    def shut_down(arg):
-        logging.debug("Shutting down")
-        if isinstance(arg, Failure):
-            logging.error("Shut down is due to an error: %s", arg.getTraceback())
-        d = defer.maybeDeferred(server_port[0].stopListening)
-        d.addErrback(print_error)
-        d.addCallback(lambda _: session.shut_down())
-        d.addCallback(lambda _: stream_info_manager.stop())
-        d.addErrback(print_error)
-        d.addCallback(lambda _: set_dead_event())
-        d.addErrback(print_error)
-        d.addCallback(lambda _: reactor.callLater(0, stop_reactor))
-        d.addErrback(print_error)
-        return d
-
-    def wait_for_kill_event():
-
-        d = defer.Deferred()
-
-        def check_for_kill():
-            if kill_event.is_set():
-                logging.debug("Kill event has been found set")
-                kill_check.stop()
-                d.callback(True)
-
-        kill_check = task.LoopingCall(check_for_kill)
-        kill_check.start(1.0)
-
-        return d
-
-    def enable_live_stream():
-        add_live_stream_to_sd_identifier(sd_identifier, session.base_payment_rate_manager)
-        add_full_live_stream_downloader_to_sd_identifier(session, stream_info_manager, sd_identifier,
-                                                         session.base_payment_rate_manager)
-
-    def run_server():
-        d = session.setup()
-        d.addCallback(lambda _: stream_info_manager.setup())
-        d.addCallback(lambda _: enable_live_stream())
-        d.addCallback(lambda _: start_listening())
-        d.addCallback(lambda _: create_stream())
-        d.addCallback(lambda _: wait_for_kill_event())
-        d.addBoth(shut_down)
-        return d
-
-    reactor.callLater(1, run_server)
     if not reactor.running:
         reactor.run()
 
@@ -702,101 +566,6 @@ class TestTransfer(TestCase):
         d.addCallback(start_transfer)
         d.addBoth(stop)
 
-        return d
-
-    @unittest.skip("Sadly skipping failing test instead of fixing it")
-    def test_live_transfer(self):
-
-        sd_hash_queue = Queue()
-        kill_event = Event()
-        dead_event = Event()
-        server_args = (sd_hash_queue, kill_event, dead_event)
-        server = Process(target=start_live_server, args=server_args)
-        server.start()
-        self.server_processes.append(server)
-
-        wallet = FakeWallet()
-        peer_manager = PeerManager()
-        peer_finder = FakePeerFinder(5553, peer_manager, 1)
-        hash_announcer = FakeAnnouncer()
-        rate_limiter = DummyRateLimiter()
-        sd_identifier = StreamDescriptorIdentifier()
-
-        db_dir = "client"
-        os.mkdir(db_dir)
-
-        self.session = Session(
-            conf.ADJUSTABLE_SETTINGS['data_rate'][1], db_dir=db_dir, lbryid="abcd",
-            peer_finder=peer_finder, hash_announcer=hash_announcer, blob_dir=None,
-            peer_port=5553, use_upnp=False, rate_limiter=rate_limiter, wallet=wallet,
-            blob_tracker_class=DummyBlobAvailabilityTracker, dht_node_class=Node
-        )
-
-        self.stream_info_manager = TempLiveStreamMetadataManager(hash_announcer)
-
-        d = self.wait_for_hash_from_queue(sd_hash_queue)
-
-        def create_downloader(metadata, prm):
-            info_validator = metadata.validator
-            options = metadata.options
-            factories = metadata.factories
-            chosen_options = [
-                o.default_value for o in options.get_downloader_options(info_validator, prm)]
-            return factories[0].make_downloader(metadata, chosen_options, prm)
-
-        def start_lbry_file(lbry_file):
-            lbry_file = lbry_file
-            return lbry_file.start()
-
-        def download_stream(sd_blob_hash):
-            prm = self.session.payment_rate_manager
-            d = download_sd_blob(self.session, sd_blob_hash, prm)
-            d.addCallback(sd_identifier.get_metadata_for_sd_blob)
-            d.addCallback(create_downloader, prm)
-            d.addCallback(start_lbry_file)
-            return d
-
-        def do_download(sd_blob_hash):
-            logging.debug("Starting the download")
-
-            d = self.session.setup()
-            d.addCallback(lambda _: enable_live_stream())
-            d.addCallback(lambda _: download_stream(sd_blob_hash))
-            return d
-
-        def enable_live_stream():
-            add_live_stream_to_sd_identifier(sd_identifier, self.session.payment_rate_manager)
-            add_full_live_stream_downloader_to_sd_identifier(self.session, self.stream_info_manager,
-                                                             sd_identifier,
-                                                             self.session.payment_rate_manager)
-
-        d.addCallback(do_download)
-
-        def check_md5_sum():
-            f = open('test_file')
-            hashsum = MD5.new()
-            hashsum.update(f.read())
-            self.assertEqual(hashsum.hexdigest(), "215b177db8eed86d028b37e5cbad55c7")
-
-        d.addCallback(lambda _: check_md5_sum())
-
-        def stop(arg):
-            if isinstance(arg, Failure):
-                logging.debug("Client is stopping due to an error. Error: %s", arg.getTraceback())
-            else:
-                logging.debug("Client is stopping normally.")
-            kill_event.set()
-            logging.debug("Set the kill event")
-            d = self.wait_for_event(dead_event, 15)
-
-            def print_shutting_down():
-                logging.info("Client is shutting down")
-
-            d.addCallback(lambda _: print_shutting_down())
-            d.addCallback(lambda _: arg)
-            return d
-
-        d.addBoth(stop)
         return d
 
     def test_last_blob_retrieval(self):
