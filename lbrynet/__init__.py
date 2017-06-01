8a7331554178d61b51b56086f0019166fadf5b34
Jack Robison <jackrobison@lbry.io>
2017-06-01 16:37:55 -0400

    Bump version: 0.10.3production4 → 0.10.4rc1

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.10.3production4"
+__version__ = "0.10.4rc1"
 version = tuple(__version__.split('.'))
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
