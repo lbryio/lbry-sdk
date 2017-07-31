4e9880ee33f871b438afd5bf2a7ef513be872e71
Kay Kurokawa <kay@lbry.io>
2017-07-31 13:55:58 -0400

    add blob_announce ability to specify lists of blobs

--- lbrynet/daemon/Daemon.py
+++ lbrynet/daemon/Daemon.py
@@ -2351,19 +2351,23 @@ class Daemon(AuthJSONRPCServer):
 
     @defer.inlineCallbacks
     @AuthJSONRPCServer.flags(announce_all="-a")
-    def jsonrpc_blob_announce(self, announce_all=None, blob_hash=None,
+    def jsonrpc_blob_announce(self, announce_all=None, blob_hash=None, blob_hashes=None,
                               stream_hash=None, sd_hash=None):
         """
         Announce blobs to the DHT
 
         Usage:
             blob_announce [-a] [<blob_hash> | --blob_hash=<blob_hash>]
+                          [<blob_hashes> | --blob_hashes=<blob_hashes>]
                           [<stream_hash> | --stream_hash=<stream_hash>]
                           [<sd_hash> | --sd_hash=<sd_hash>]
 
         Options:
             -a                                          : announce all the blobs possessed by user
-            <blob_hash>, --blob_hash=<blob_hash>        : announce a blob, specified by blob_hash
+            <blob_hash>, --blob_hash=<blob_hash>        : announce a single blob, specified by
+                                                            blob_hash
+            <blob_hashes>, --blob_hashes=<blob_hashes>  : announce a list of blobs, specified by
+                                                            their blob_hashes
             <stream_hash>, --stream_hash=<stream_hash>  : announce all blobs associated with
                                                             stream_hash
             <sd_hash>, --sd_hash=<sd_hash>              : announce all blobs associated with
@@ -2376,6 +2380,8 @@ class Daemon(AuthJSONRPCServer):
             yield self.session.blob_manager.immediate_announce_all_blobs()
         elif blob_hash:
             blob_hashes = [blob_hash]
+            yield self.session.blob_manager._immediate_announce(blob_hashes)
+        elif blob_hashes:
             yield self.session.blob_manager._immediate_announce(blob_hashes)
         elif stream_hash:
             blobs = yield self.get_blobs_for_stream_hash(stream_hash)
