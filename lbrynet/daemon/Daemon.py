f83e7b3e73d9df2457c99712da56468ba6a07432
Jack Robison <jackrobison@lbry.io>
2017-09-28 13:52:30 -0400

    json blobs

--- lbrynet/daemon/Daemon.py
+++ lbrynet/daemon/Daemon.py
@@ -640,7 +640,7 @@ class Daemon(AuthJSONRPCServer):
             host = yield self.session.blob_manager.get_host_downloaded_from(blob_hash)
             if host:
                 blobs[blob_num] = host
-        report["blobs"] = blobs
+        report["blobs"] = json.dumps(blobs)
         defer.returnValue(report)
 
     @defer.inlineCallbacks
