c9fe544a0108db5038f694360171e2b1c9427141
Kamil Piekutowski <influence1982@gmail.com>
2017-10-10 22:50:54 -0500

    more cleanup

--- lbrynet/daemon/MediaDescriptor.py
+++ lbrynet/daemon/MediaDescriptor.py
@@ -1,14 +1,14 @@
 import subprocess
- 
-class Duration:
-    def __init__(self, time):
+
+class Duration(self, time):
         (self.hours, self.minutes, self.seconds) = time
     def all_in_millisec(self):
-        return (int((float(self.hours) * 3600)  + (float(self.minutes) * 60) + (float(self.seconds))) * 1000); 
-        
+        return (int((float(self.hours) * 3600) +
+               (float(self.minutes) * 60) + (float(self.seconds))) * 1000)
 
 def get_video_length(path):
-    process = subprocess.Popen(['/usr/bin/ffmpeg', '-i', path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process = subprocess.Popen(['/usr/bin/ffmpeg', '-i', path],
+              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = process.communicate()
 
     for line in stdout.splitlines():
