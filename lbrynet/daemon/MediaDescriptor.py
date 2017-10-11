57a702dfdd62cdb1fd68f9fefdd42ad0b1ad6865
Kamil Piekutowski <influence1982@gmail.com>
2017-10-10 23:08:26 -0500

    switched to new style python class in MediaDescriptor

--- lbrynet/daemon/MediaDescriptor.py
+++ lbrynet/daemon/MediaDescriptor.py
@@ -1,6 +1,7 @@
 import subprocess
 
-class Duration(self, time):
+class Duration(object):
+    def __init__(self, time):
         (self.hours, self.minutes, self.seconds) = time
     def all_in_millisec(self):
         return (int((float(self.hours) * 3600) +
@@ -10,7 +11,6 @@ def get_video_length(path):
     process = subprocess.Popen(['/usr/bin/ffmpeg', '-i', path],
               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = process.communicate()
-
     for line in stdout.splitlines():
         if 'Duration' in line:
             line = line.replace(',', '')
