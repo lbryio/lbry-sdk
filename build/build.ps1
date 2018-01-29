e5f2692ccb605fadbe233121766e1f3623f47af5
Antonio Quartulli <antonio@mandelbit.com>
2018-01-29 10:00:38 +0800

    MinGW: update to miniupnpc-2.0.2
    Signed-off-by: Antonio Quartulli <antonio@mandelbit.com>

--- build/build.ps1
+++ build/build.ps1
@@ -5,13 +5,13 @@ gcc --version
 mingw32-make --version
 
 # build/install miniupnpc manually
-tar zxf miniupnpc-1.9.tar.gz
-cd miniupnpc-1.9
+tar zxf miniupnpc-2.0.2.tar.gz
+cd miniupnpc-2.0.2
 mingw32-make -f Makefile.mingw
 python setupmingw32.py build --compiler=mingw32
 python setupmingw32.py install
 cd ..\
-Remove-Item -Recurse -Force miniupnpc-1.9
+Remove-Item -Recurse -Force miniupnpc-2.0.2
 
 # copy requirements from lbry, but remove miniupnpc (installed manually)
 Get-Content ..\requirements.txt | Select-String -Pattern 'miniupnpc' -NotMatch | Out-File requirements_base.txt
