1327757e35d266fed8e7d904f873ef18cb1be38b
Jack Robison <jackrobison@lbry.io>
2018-01-31 19:52:24 -0500

    Bump version 0.19.0rc30 --> 0.19.0rc31
    Signed-off-by: Jack Robison <jackrobison@lbry.io>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
     'envparse',
     'jsonrpc',
     'jsonschema',
-    'lbryum==3.2.0rc12',
+    'lbryum==3.2.0rc13',
     'lbryschema==0.0.15rc2',
     'miniupnpc',
     'pycrypto',
