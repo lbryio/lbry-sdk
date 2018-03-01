c7bfb0a2e75f51c5144be5ec892a77b13223f244
hackrush <atulshyan96@gmail.com>
2018-03-01 21:42:30 +0530

    ImportError checking and kill process after completion

--- scripts/gen_docs.py
+++ scripts/gen_docs.py
@@ -1,10 +1,25 @@
-import gen_cli_docs
-import gen_api_docs
 import os.path as op
 import subprocess
+
+try:
+  import mkdocs
+except ImportError:
+  raise ImportError("mkdocs is not installed")
+
+try:
+  import tabulate
+except ImportError:
+  raise ImportError("tabulate is not installed")
+
+try:
+  import gen_cli_docs
+  import gen_api_docs
+except ImportError:
+  raise ImportError("Probably not inside the lbry's virtual environment or daemon not installed")
 
 gen_cli_docs.main()
 gen_api_docs.main()
 cwd = op.dirname(op.realpath(__file__))
 cwd = op.realpath(op.join(cwd, ".."))
-proc = subprocess.Popen("mkdocs build", cwd=cwd, shell=True)
+proc = subprocess.Popen("exec mkdocs build", cwd=cwd, shell=True)
+proc.kill()
