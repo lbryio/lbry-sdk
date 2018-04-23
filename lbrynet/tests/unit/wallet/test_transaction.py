83958604d50c37b9b6ad2f40cdd6c4924024be51
Lex Berezhny <lex@damoti.com>
2018-04-22 22:23:42 -0400

    finally generating fully signed tx submittable to lbrycrd

---
+++ lbrynet/tests/unit/wallet/test_transaction.py
@@ -0,0 +1,229 @@
+from binascii import hexlify, unhexlify
+from twisted.trial import unittest
+from lbrynet.wallet.constants import CENT
+from lbrynet.wallet.transaction import Transaction, Input, Output
+from lbrynet.wallet.manager import WalletManager
+from lbrynet.wallet import set_wallet_manager
+from lbrynet.wallet.bip32 import PrivateKey
+from lbrynet.wallet.mnemonic import Mnemonic
+
+
+NULL_HASH = '\x00'*32
+FEE_PER_BYTE = 50
+FEE_PER_CHAR = 200000
+
+
+class TestSizeAndFeeEstimation(unittest.TestCase):
+
+    def setUp(self):
+        set_wallet_manager(WalletManager({
+            'fee_per_byte': FEE_PER_BYTE,
+            'fee_per_name_char': FEE_PER_CHAR
+        }))
+
+    @staticmethod
+    def get_output():
+        return Output.pay_pubkey_hash(Transaction(), 1, CENT, NULL_HASH)
+
+    @classmethod
+    def get_input(cls):
+        return cls.get_output().spend(fake=True)
+
+    @classmethod
+    def get_transaction(cls):
+        tx = Transaction()
+        Output.pay_pubkey_hash(tx, 1, CENT, NULL_HASH)
+        tx.add_inputs([cls.get_input()])
+        return tx
+
+    @classmethod
+    def get_claim_transaction(cls, claim_name, claim=''):
+        tx = Transaction()
+        Output.pay_claim_name_pubkey_hash(tx, 1, CENT, claim_name, claim, NULL_HASH)
+        tx.add_inputs([cls.get_input()])
+        return tx
+
+    def test_output_size_and_fee(self):
+        txo = self.get_output()
+        self.assertEqual(txo.size, 46)
+        self.assertEqual(txo.fee, 46 * FEE_PER_BYTE)
+
+    def test_input_size_and_fee(self):
+        txi = self.get_input()
+        self.assertEqual(txi.size, 148)
+        self.assertEqual(txi.fee, 148 * FEE_PER_BYTE)
+
+    def test_transaction_size_and_fee(self):
+        tx = self.get_transaction()
+        base_size = tx.size - 1 - tx.inputs[0].size
+        self.assertEqual(tx.size, 204)
+        self.assertEqual(tx.base_size, base_size)
+        self.assertEqual(tx.base_fee, FEE_PER_BYTE * base_size)
+
+    def test_claim_name_transaction_size_and_fee(self):
+        # fee based on claim name is the larger fee
+        claim_name = 'verylongname'
+        tx = self.get_claim_transaction(claim_name, '0'*4000)
+        base_size = tx.size - 1 - tx.inputs[0].size
+        self.assertEqual(tx.size, 4225)
+        self.assertEqual(tx.base_size, base_size)
+        self.assertEqual(tx.base_fee, len(claim_name) * FEE_PER_CHAR)
+        # fee based on total bytes is the larger fee
+        claim_name = 'a'
+        tx = self.get_claim_transaction(claim_name, '0'*4000)
+        base_size = tx.size - 1 - tx.inputs[0].size
+        self.assertEqual(tx.size, 4214)
+        self.assertEqual(tx.base_size, base_size)
+        self.assertEqual(tx.base_fee, FEE_PER_BYTE * base_size)
+
+
+class TestTransactionSerialization(unittest.TestCase):
+
+    def test_genesis_transaction(self):
+        raw = unhexlify(
+            "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff1f0"
+            "4ffff001d010417696e736572742074696d657374616d7020737472696e67ffffffff01000004bfc91b8e"
+            "001976a914345991dbf57bfb014b87006acdfafbfc5fe8292f88ac00000000"
+        )
+        tx = Transaction(raw)
+        self.assertEqual(tx.version, 1)
+        self.assertEqual(tx.locktime, 0)
+        self.assertEqual(len(tx.inputs), 1)
+        self.assertEqual(len(tx.outputs), 1)
+
+        coinbase = tx.inputs[0]
+        self.assertEqual(coinbase.output_tx_hash, NULL_HASH)
+        self.assertEqual(coinbase.output_index, 0xFFFFFFFF)
+        self.assertEqual(coinbase.sequence, 0xFFFFFFFF)
+        self.assertTrue(coinbase.is_coinbase)
+        self.assertEqual(coinbase.script, None)
+        self.assertEqual(
+            hexlify(coinbase.coinbase),
+            b'04ffff001d010417696e736572742074696d657374616d7020737472696e67'
+        )
+
+        out = tx.outputs[0]
+        self.assertEqual(out.amount, 40000000000000000)
+        self.assertEqual(out.index, 0)
+        self.assertTrue(out.script.is_pay_pubkey_hash)
+        self.assertFalse(out.script.is_pay_script_hash)
+        self.assertFalse(out.script.is_claim_involved)
+
+        tx._reset()
+        self.assertEqual(tx.raw, raw)
+
+    def test_coinbase_transaction(self):
+        raw = unhexlify(
+            "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff200"
+            "34d520504f89ac55a086032d217bf0700000d2f6e6f64655374726174756d2f0000000001a03489850800"
+            "00001976a914cfab870d6deea54ca94a41912a75484649e52f2088ac00000000"
+        )
+        tx = Transaction(raw)
+        self.assertEqual(tx.version, 1)
+        self.assertEqual(tx.locktime, 0)
+        self.assertEqual(len(tx.inputs), 1)
+        self.assertEqual(len(tx.outputs), 1)
+
+        coinbase = tx.inputs[0]
+        self.assertEqual(coinbase.output_tx_hash, NULL_HASH)
+        self.assertEqual(coinbase.output_index, 0xFFFFFFFF)
+        self.assertEqual(coinbase.sequence, 0)
+        self.assertTrue(coinbase.is_coinbase)
+        self.assertEqual(coinbase.script, None)
+        self.assertEqual(
+            hexlify(coinbase.coinbase),
+            b'034d520504f89ac55a086032d217bf0700000d2f6e6f64655374726174756d2f'
+        )
+
+        out = tx.outputs[0]
+        self.assertEqual(out.amount, 36600100000)
+        self.assertEqual(out.index, 0)
+        self.assertTrue(out.script.is_pay_pubkey_hash)
+        self.assertFalse(out.script.is_pay_script_hash)
+        self.assertFalse(out.script.is_claim_involved)
+
+        tx._reset()
+        self.assertEqual(tx.raw, raw)
+
+    def test_claim_transaction(self):
+        raw = unhexlify(
+            "01000000012433e1b327603843b083344dbae5306ff7927f87ebbc5ae9eb50856c5b53fd1d000000006a4"
+            "7304402201a91e1023d11c383a11e26bf8f9034087b15d8ada78fa565e0610455ffc8505e0220038a63a6"
+            "ecb399723d4f1f78a20ddec0a78bf8fb6c75e63e166ef780f3944fbf0121021810150a2e4b088ec51b20c"
+            "be1b335962b634545860733367824d5dc3eda767dffffffff028096980000000000fdff00b50463617473"
+            "4cdc080110011a7808011230080410011a084d616361726f6e6922002a003214416c6c207269676874732"
+            "072657365727665642e38004a0052005a001a42080110011a30add80aaf02559ba09853636a0658c42b72"
+            "7cb5bb4ba8acedb4b7fe656065a47a31878dbf9912135ddb9e13806cc1479d220a696d6167652f6a70656"
+            "72a5c080110031a404180cc0fa4d3839ee29cca866baed25fafb43fca1eb3b608ee889d351d3573d042c7"
+            "b83e2e643db0d8e062a04e6e9ae6b90540a2f95fe28638d0f18af4361a1c2214f73de93f4299fb32c32f9"
+            "49e02198a8e91101abd6d7576a914be16e4b0f9bd8f6d47d02b3a887049c36d3b84cb88ac0cd2520b0000"
+            "00001976a914f521178feb733a719964e1da4a9efb09dcc39cfa88ac00000000"
+        )
+        tx = Transaction(raw)
+        self.assertEqual(hexlify(tx.id), b'666c3d15de1d6949a4fe717126c368e274b36957dce29fd401138c1e87e92a62')
+        self.assertEqual(tx.version, 1)
+        self.assertEqual(tx.locktime, 0)
+        self.assertEqual(len(tx.inputs), 1)
+        self.assertEqual(len(tx.outputs), 2)
+
+        txin = tx.inputs[0]  # type: Input
+        self.assertEqual(
+            hexlify(txin.output_tx_hash[::-1]),
+            b'1dfd535b6c8550ebe95abceb877f92f76f30e5ba4d3483b043386027b3e13324'
+        )
+        self.assertEqual(txin.output_index, 0)
+        self.assertEqual(txin.sequence, 0xFFFFFFFF)
+        self.assertFalse(txin.is_coinbase)
+        self.assertEqual(txin.script.template.name, 'pubkey_hash')
+        self.assertEqual(
+            hexlify(txin.script.values['pubkey']),
+            b'021810150a2e4b088ec51b20cbe1b335962b634545860733367824d5dc3eda767d'
+        )
+        self.assertEqual(
+            hexlify(txin.script.values['signature']),
+            b'304402201a91e1023d11c383a11e26bf8f9034087b15d8ada78fa565e0610455ffc8505e0220038a63a6'
+            b'ecb399723d4f1f78a20ddec0a78bf8fb6c75e63e166ef780f3944fbf01'
+        )
+
+        # Claim
+        out0 = tx.outputs[0]  # type: Output
+        self.assertEqual(out0.amount, 10000000)
+        self.assertEqual(out0.index, 0)
+        self.assertTrue(out0.script.is_pay_pubkey_hash)
+        self.assertTrue(out0.script.is_claim_name)
+        self.assertTrue(out0.script.is_claim_involved)
+        self.assertEqual(out0.script.values['claim_name'], b'cats')
+        self.assertEqual(
+            hexlify(out0.script.values['pubkey_hash']),
+            b'be16e4b0f9bd8f6d47d02b3a887049c36d3b84cb'
+        )
+
+        # Change
+        out1 = tx.outputs[1]  # type: Output
+        self.assertEqual(out1.amount, 189977100)
+        self.assertEqual(out1.index, 1)
+        self.assertTrue(out1.script.is_pay_pubkey_hash)
+        self.assertFalse(out1.script.is_claim_involved)
+        self.assertEqual(
+            hexlify(out1.script.values['pubkey_hash']),
+            b'f521178feb733a719964e1da4a9efb09dcc39cfa'
+        )
+
+        tx._reset()
+        self.assertEqual(tx.raw, raw)
+
+
+class TestTransactionSigning(unittest.TestCase):
+
+    def setUp(self):
+        self.private_key = PrivateKey.from_seed(Mnemonic.mnemonic_to_seed(
+           'program leader library giant team normal suspect crater pair miracle sweet until absent'
+        ))
+
+    def test_sign(self):
+        tx = Transaction()
+        Output.pay_pubkey_hash(Transaction(), 0, CENT, NULL_HASH).spend(fake=True)
+        tx.add_inputs([self.get_input()])
+        Output.pay_pubkey_hash(tx, 0, CENT, NULL_HASH)
+        tx = self.get_tx()
+
--- lbrynet/wallet/account.py
+++ lbrynet/wallet/account.py
@@ -1,75 +1,141 @@
-import logging
+from binascii import hexlify, unhexlify
+from itertools import chain
+from lbrynet.wallet import get_wallet_manager
+from lbrynet.wallet.mnemonic import Mnemonic
+from lbrynet.wallet.bip32 import PrivateKey, PubKey, from_extended_key_string
+from lbrynet.wallet.hash import double_sha256, aes_encrypt, aes_decrypt
 
 from lbryschema.address import public_key_to_address
 
-from .lbrycrd import deserialize_xkey
-from .lbrycrd import CKD_pub
 
-log = logging.getLogger(__name__)
+class KeyChain:
 
-
-def get_key_chain_from_xpub(xpub):
-    _, _, _, chain, key = deserialize_xkey(xpub)
-    return key, chain
-
-
-class AddressSequence:
-
-    def __init__(self, derived_keys, gap, age_checker, pub_key, chain_key):
-        self.gap = gap
-        self.is_old = age_checker
-        self.pub_key = pub_key
-        self.chain_key = chain_key
-        self.derived_keys = derived_keys
+    def __init__(self, parent_key, child_keys, gap):
+        self.parent_key = parent_key  # type: PubKey
+        self.child_keys = child_keys
+        self.minimum_gap = gap
         self.addresses = [
-            public_key_to_address(key.decode('hex'))
-            for key in derived_keys
+            public_key_to_address(key)
+            for key in child_keys
         ]
 
-    def generate_next_address(self):
-        new_key, _ = CKD_pub(self.pub_key, self.chain_key, len(self.derived_keys))
-        address = public_key_to_address(new_key)
-        self.derived_keys.append(new_key.encode('hex'))
-        self.addresses.append(address)
-        return address
-
+    @property
     def has_gap(self):
-        if len(self.addresses) < self.gap:
+        if len(self.addresses) < self.minimum_gap:
             return False
-        for address in self.addresses[-self.gap:]:
-            if self.is_old(address):
+        ledger = get_wallet_manager().ledger
+        for address in self.addresses[-self.minimum_gap:]:
+            if ledger.is_address_old(address):
                 return False
         return True
 
+    def generate_next_address(self):
+        child_key = self.parent_key.child(len(self.child_keys))
+        self.child_keys.append(child_key.pubkey_bytes)
+        self.addresses.append(child_key.address)
+        return child_key.address
+
     def ensure_enough_addresses(self):
         starting_length = len(self.addresses)
-        while not self.has_gap():
+        while not self.has_gap:
             self.generate_next_address()
         return self.addresses[starting_length:]
 
 
 class Account:
 
-    def __init__(self, data, receiving_gap, change_gap, age_checker):
-        self.xpub = data['xpub']
-        master_key, master_chain = get_key_chain_from_xpub(data['xpub'])
-        self.receiving = AddressSequence(
-            data.get('receiving', []), receiving_gap, age_checker,
-            *CKD_pub(master_key, master_chain, 0)
-        )
-        self.change = AddressSequence(
-            data.get('change', []), change_gap, age_checker,
-            *CKD_pub(master_key, master_chain, 1)
-        )
-        self.is_old = age_checker
+    def __init__(self, seed, encrypted, private_key, public_key, **kwargs):
+        self.seed = seed
+        self.encrypted = encrypted
+        self.private_key = private_key  # type: PrivateKey
+        self.public_key = public_key  # type: PubKey
+        self.receiving_gap = kwargs.get('receiving_gap', 20)
+        self.receiving_keys = kwargs.get('receiving_keys') or \
+            KeyChain(self.public_key.child(0), [], self.receiving_gap)
+        self.change_gap = kwargs.get('change_gap', 6)
+        self.change_keys = kwargs.get('change_keys') or \
+            KeyChain(self.public_key.child(1), [], self.change_gap)
+        self.keychains = [
+            self.receiving_keys,  # child: 0
+            self.change_keys      # child: 1
+        ]
 
-    def as_dict(self):
+    @classmethod
+    def generate(cls):
+        seed = Mnemonic().make_seed()
+        return cls.generate_from_seed(seed)
+
+    @classmethod
+    def generate_from_seed(cls, seed):
+        private_key = cls.get_private_key_from_seed(seed)
+        return cls(
+            seed=seed, encrypted=False,
+            private_key=private_key,
+            public_key=private_key.public_key,
+        )
+
+    @classmethod
+    def from_json(cls, json_data):
+        data = json_data.copy()
+        if not data['encrypted']:
+            data['private_key'] = from_extended_key_string(data['private_key'])
+        data['public_key'] = from_extended_key_string(data['public_key'])
+        data['receiving_keys'] = KeyChain(
+            data['public_key'].child(0),
+            [unhexlify(k) for k in data['receiving_keys']],
+            data['receiving_gap']
+        )
+        data['change_keys'] = KeyChain(
+            data['public_key'].child(1),
+            [unhexlify(k) for k in data['change_keys']],
+            data['change_gap']
+        )
+        return cls(**data)
+
+    def to_json(self):
         return {
-            'receiving': self.receiving.derived_keys,
-            'change': self.change.derived_keys,
-            'xpub': self.xpub
+            'seed': self.seed,
+            'encrypted': self.encrypted,
+            'private_key': self.private_key.extended_key_string(),
+            'public_key': self.public_key.extended_key_string(),
+            'receiving_keys': [hexlify(k) for k in self.receiving_keys.child_keys],
+            'receiving_gap': self.receiving_gap,
+            'change_keys': [hexlify(k) for k in self.change_keys.child_keys],
+            'change_gap': self.change_gap
         }
 
+    def decrypt(self, password):
+        assert self.encrypted, "Key is not encrypted."
+        secret = double_sha256(password)
+        self.seed = aes_decrypt(secret, self.seed)
+        self.private_key = from_extended_key_string(aes_decrypt(secret, self.private_key))
+        self.encrypted = False
+
+    def encrypt(self, password):
+        assert not self.encrypted, "Key is already encrypted."
+        secret = double_sha256(password)
+        self.seed = aes_encrypt(secret, self.seed)
+        self.private_key = aes_encrypt(secret, self.private_key.extended_key_string())
+        self.encrypted = True
+
+    @staticmethod
+    def get_private_key_from_seed(seed):
+        return PrivateKey.from_seed(Mnemonic.mnemonic_to_seed(seed))
+
     @property
-    def sequences(self):
-        return self.receiving, self.change
+    def addresses(self):
+        return chain(self.receiving_keys.addresses, self.change_keys.addresses)
+
+    def get_private_key_for_address(self, address):
+        assert not self.encrypted, "Cannot get private key on encrypted wallet account."
+        for a, keychain in enumerate(self.keychains):
+            for b, match in enumerate(keychain.addresses):
+                if address == match:
+                    return self.private_key.child(a).child(b)
+
+    def ensure_enough_addresses(self):
+        return [
+            address
+            for keychain in self.keychains
+            for address in keychain.ensure_enough_addresses()
+        ]
