b770b2de6a705838e21665b7672239532522ef83
Sergey Rozhnov <smr.oznob@gmail.com>
2018-05-17 13:36:32 +0400

    simplified claims comparison logic; refactored unit tests

--- lbrynet/daemon/claims_comparator.py
+++ lbrynet/daemon/claims_comparator.py
@@ -1,36 +1,10 @@
-_comparison_order = ['height', 'name', 'claim_id']  # TODO outpoint
-
-
 def arrange_results(claims):
     for claim in claims:
         results = claim['result']
-        sorted_results = sorted(results, cmp=_compare_results)
+        sorted_results = sorted(results, key=lambda d: (d['height'], d['name'], d['claim_id'], _outpoint(d)))
         claim['result'] = sorted_results
     return claims
 
 
-def _compare_results(left, right):
-    """
-    :type left: dict
-    :type right: dict
-    """
-    result = 0
-
-    for attribute in _comparison_order:
-        left_value = left[attribute]
-        right_value = right[attribute]
-        sub_result = _cmp(left_value, right_value)
-        if sub_result is not 0:
-            result = sub_result
-            break
-
-    return result
-
-
-def _cmp(left, right):
-    if left == right:
-        return 0
-    elif left < right:
-        return -1
-    else:
-        return 1
+def _outpoint(claim):
+    return '{}:{}'.format(claim['txid'], claim['nout'])
