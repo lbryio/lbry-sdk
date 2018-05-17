332b3e1fefaadb3e6b45ed77fcb5c99357aa66d3
Sergey Rozhnov <smr.oznob@gmail.com>
2018-05-17 13:36:32 +0400

    simplified claims comparison logic; refactored unit tests

--- lbrynet/tests/unit/daemon/test_claims_comparator.py
+++ lbrynet/tests/unit/daemon/test_claims_comparator.py
@@ -1,51 +1,26 @@
+import json
 import unittest
 
 from lbrynet.daemon.claims_comparator import arrange_results
 
 
 class ClaimsComparatorTest(unittest.TestCase):
+    def setUp(self):
+        with open('claims_comparator_cases.json') as f:
+            document = json.load(f)
+        self.cases = document['cases']
+
     def test_arrange_results(self):
-        results = [
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'ccc'
-            },
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'aaa'
-            },
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'bbb'
-            }
-        ]
-        data = {'result': results}
+        for case in self.cases:
+            results = case['results']
+            data = {'result': results}
+            expected = case['expected']
 
-        expected = [
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'aaa'
-            },
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'bbb'
-            },
-            {
-                'height': 1,
-                'name': 'res',
-                'claim_id': 'ccc'
-            }
-        ]
-        claims = arrange_results([data])
-        claim = claims[0]
-        actual = claim['result']
+            claims = arrange_results([data])
+            claim = claims[0]
+            actual = claim['result']
 
-        self.assertEqual(expected, actual)
+            self.assertEqual(expected, actual, case['description'])
 
 
 if __name__ == '__main__':
