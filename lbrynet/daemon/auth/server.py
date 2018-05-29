9b1e687b9bacbbfd3b46589a436deaeda9df95bf
Jack Robison <jackrobison@lbry.io>
2018-05-29 14:36:00 -0400

    pylint

--- lbrynet/daemon/auth/server.py
+++ lbrynet/daemon/auth/server.py
@@ -13,7 +13,7 @@ from txjsonrpc import jsonrpclib
 from traceback import format_exc
 
 from lbrynet import conf
-from lbrynet.core.Error import InvalidAuthenticationToken, InvalidHeaderError
+from lbrynet.core.Error import InvalidAuthenticationToken
 from lbrynet.core import utils
 from lbrynet.daemon.auth.util import APIKey, get_auth_message
 from lbrynet.daemon.auth.client import LBRY_SECRET
