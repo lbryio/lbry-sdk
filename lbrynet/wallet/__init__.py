f8cc73c7d25ff61f5104cdcfd6d3eb0fc00aa305
Lex Berezhny <lex@damoti.com>
2018-06-14 00:53:38 -0400

    updated to newer torba

--- lbrynet/wallet/__init__.py
+++ lbrynet/wallet/__init__.py
@@ -1,4 +1,9 @@
-__coin__ = 'LBC'
+__node_daemon__ = 'lbrycrdd'
+__node_cli__ = 'lbrycrd-cli'
+__node_bin__ = ''
+__node_url__ = (
+    'https://github.com/lbryio/lbrycrd/releases/download/v0.12.2.1/lbrycrd-linux.zip'
+)
+__electrumx__ = 'lbryumx.coin.LBCRegTest'
 
-from .coin import LBC, LBCRegTest
-from .manager import LbryWalletManager
+from .ledger import MainNetLedger, RegTestLedger
---
+++ lbrynet/wallet/script.py
@@ -0,0 +1,80 @@
+from torba.basescript import BaseInputScript, BaseOutputScript, Template
+from torba.basescript import PUSH_SINGLE, OP_DROP, OP_2DROP
+
+
+class InputScript(BaseInputScript):
+    pass
+
+
+class OutputScript(BaseOutputScript):
+
+    # lbry custom opcodes
+    OP_CLAIM_NAME = 0xb5
+    OP_SUPPORT_CLAIM = 0xb6
+    OP_UPDATE_CLAIM = 0xb7
+
+    CLAIM_NAME_OPCODES = (
+        OP_CLAIM_NAME, PUSH_SINGLE('claim_name'), PUSH_SINGLE('claim'),
+        OP_2DROP, OP_DROP
+    )
+    CLAIM_NAME_PUBKEY = Template('claim_name+pay_pubkey_hash', (
+            CLAIM_NAME_OPCODES + BaseOutputScript.PAY_PUBKEY_HASH.opcodes
+    ))
+    CLAIM_NAME_SCRIPT = Template('claim_name+pay_script_hash', (
+            CLAIM_NAME_OPCODES + BaseOutputScript.PAY_SCRIPT_HASH.opcodes
+    ))
+
+    SUPPORT_CLAIM_OPCODES = (
+        OP_SUPPORT_CLAIM, PUSH_SINGLE('claim_name'), PUSH_SINGLE('claim_id'),
+        OP_2DROP, OP_DROP
+    )
+    SUPPORT_CLAIM_PUBKEY = Template('support_claim+pay_pubkey_hash', (
+            SUPPORT_CLAIM_OPCODES + BaseOutputScript.PAY_PUBKEY_HASH.opcodes
+    ))
+    SUPPORT_CLAIM_SCRIPT = Template('support_claim+pay_script_hash', (
+            SUPPORT_CLAIM_OPCODES + BaseOutputScript.PAY_SCRIPT_HASH.opcodes
+    ))
+
+    UPDATE_CLAIM_OPCODES = (
+        OP_UPDATE_CLAIM, PUSH_SINGLE('claim_name'), PUSH_SINGLE('claim_id'), PUSH_SINGLE('claim'),
+        OP_2DROP, OP_2DROP
+    )
+    UPDATE_CLAIM_PUBKEY = Template('update_claim+pay_pubkey_hash', (
+            UPDATE_CLAIM_OPCODES + BaseOutputScript.PAY_PUBKEY_HASH.opcodes
+    ))
+    UPDATE_CLAIM_SCRIPT = Template('update_claim+pay_script_hash', (
+            UPDATE_CLAIM_OPCODES + BaseOutputScript.PAY_SCRIPT_HASH.opcodes
+    ))
+
+    templates = BaseOutputScript.templates + [
+        CLAIM_NAME_PUBKEY,
+        CLAIM_NAME_SCRIPT,
+        SUPPORT_CLAIM_PUBKEY,
+        SUPPORT_CLAIM_SCRIPT,
+        UPDATE_CLAIM_PUBKEY,
+        UPDATE_CLAIM_SCRIPT
+    ]
+
+    @classmethod
+    def pay_claim_name_pubkey_hash(cls, claim_name, claim, pubkey_hash):
+        return cls(template=cls.CLAIM_NAME_PUBKEY, values={
+            'claim_name': claim_name,
+            'claim': claim,
+            'pubkey_hash': pubkey_hash
+        })
+
+    @property
+    def is_claim_name(self):
+        return self.template.name.startswith('claim_name+')
+
+    @property
+    def is_support_claim(self):
+        return self.template.name.startswith('support_claim+')
+
+    @property
+    def is_update_claim(self):
+        return self.template.name.startswith('update_claim+')
+
+    @property
+    def is_claim_involved(self):
+        return self.is_claim_name or self.is_support_claim or self.is_update_claim
