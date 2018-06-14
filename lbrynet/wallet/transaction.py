03090a8331aaefa487cd51ab42f64c56e865070f
Lex Berezhny <lex@damoti.com>
2018-06-14 15:18:36 -0400

    claim test

--- lbrynet/wallet/transaction.py
+++ lbrynet/wallet/transaction.py
@@ -1,8 +1,14 @@
 import struct
+from binascii import hexlify
+from typing import List
 
+from twisted.internet import defer
+
+from torba.baseaccount import BaseAccount
 from torba.basetransaction import BaseTransaction, BaseInput, BaseOutput
 from torba.hash import hash160
 
+from lbryschema.claim import ClaimDict
 from .script import InputScript, OutputScript
 
 
@@ -32,3 +38,12 @@ class Transaction(BaseTransaction):
         output = self.outputs[output_index]  # type: Output
         assert output.script.is_claim_name(), 'Not a name claim.'
         return claim_id_hash(self.hash, output_index)
+
+    @classmethod
+    def claim(cls, name, meta, amount, holding_address, funding_accounts, change_account):
+        # type: (bytes, ClaimDict, int, bytes, List[BaseAccount], BaseAccount) -> defer.Deferred
+        ledger = cls.ensure_all_have_same_ledger(funding_accounts, change_account)
+        claim_output = Output.pay_claim_name_pubkey_hash(
+            amount, name, hexlify(meta.serialized), ledger.address_to_hash160(holding_address)
+        )
+        return cls.pay([claim_output], funding_accounts, change_account)
