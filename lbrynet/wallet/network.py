3b46d1ad30a4a04435c0e1e73d7ce95270da03ed
Lex Berezhny <lex@damoti.com>
2018-06-20 17:25:32 -0400

    fix resolve

--- lbrynet/wallet/network.py
+++ lbrynet/wallet/network.py
@@ -3,5 +3,5 @@ from torba.basenetwork import BaseNetwork
 
 class Network(BaseNetwork):
 
-    def get_values_for_uris(self, uris):
-        return self.rpc('blockchain.claimtrie.getvaluesforuris', uris)
+    def get_values_for_uris(self, block_hash, uris):
+        return self.rpc('blockchain.claimtrie.getvaluesforuris', block_hash, uris)
