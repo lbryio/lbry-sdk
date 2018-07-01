b3e9240aa84f8514d9fb91d146056bcaf11be6c5
Lex Berezhny <lex@damoti.com>
2018-07-01 17:21:18 -0400

    wip, valuesforuris

--- lbrynet/wallet/network.py
+++ lbrynet/wallet/network.py
@@ -3,5 +3,5 @@ from torba.basenetwork import BaseNetwork
 
 class Network(BaseNetwork):
 
-    def get_values_for_uris(self, block_hash, uris):
-        return self.rpc('blockchain.claimtrie.getvaluesforuris', block_hash, uris)
+    def get_values_for_uris(self, block_hash, *uris):
+        return self.rpc('blockchain.claimtrie.getvaluesforuris', block_hash, *uris)
