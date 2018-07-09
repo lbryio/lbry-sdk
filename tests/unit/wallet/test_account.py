5d0370d1d4630e9fb706e9875ccff59562126b6a
Lex Berezhny <lex@damoti.com>
2018-07-09 17:04:59 -0400

    unit test fixes, balance/utxos filters out claims, abandoning claims

--- tests/unit/wallet/test_account.py
+++ tests/unit/wallet/test_account.py
@@ -1,62 +1,68 @@
 from twisted.trial import unittest
+from twisted.internet import defer
 
-from lbrynet.wallet import LBC
-from lbrynet.wallet.manager import LbryWalletManager
-from torba.wallet import Account
+from lbrynet.wallet.ledger import MainNetLedger
+from lbrynet.wallet.account import Account
 
 
 class TestAccount(unittest.TestCase):
 
     def setUp(self):
-        ledger = LbryWalletManager().get_or_create_ledger(LBC.get_id())
-        self.coin = LBC(ledger)
+        self.ledger = MainNetLedger(db=MainNetLedger.database_class(':memory:'))
+        return self.ledger.db.start()
 
+    @defer.inlineCallbacks
     def test_generate_account(self):
-        account = Account.generate(self.coin, u'lbryum')
-        self.assertEqual(account.coin, self.coin)
+        account = Account.generate(self.ledger, u'lbryum')
+        self.assertEqual(account.ledger, self.ledger)
         self.assertIsNotNone(account.seed)
-        self.assertEqual(account.public_key.coin, self.coin)
+        self.assertEqual(account.public_key.ledger, self.ledger)
         self.assertEqual(account.private_key.public_key, account.public_key)
 
-        self.assertEqual(len(account.receiving_keys.child_keys), 0)
-        self.assertEqual(len(account.receiving_keys.addresses), 0)
-        self.assertEqual(len(account.change_keys.child_keys), 0)
-        self.assertEqual(len(account.change_keys.addresses), 0)
+        self.assertEqual(account.public_key.ledger, self.ledger)
+        self.assertEqual(account.private_key.public_key, account.public_key)
 
-        account.ensure_enough_addresses()
-        self.assertEqual(len(account.receiving_keys.child_keys), 20)
-        self.assertEqual(len(account.receiving_keys.addresses), 20)
-        self.assertEqual(len(account.change_keys.child_keys), 6)
-        self.assertEqual(len(account.change_keys.addresses), 6)
+        addresses = yield account.receiving.get_addresses()
+        self.assertEqual(len(addresses), 0)
+        addresses = yield account.change.get_addresses()
+        self.assertEqual(len(addresses), 0)
 
+        yield account.ensure_address_gap()
+
+        addresses = yield account.receiving.get_addresses()
+        self.assertEqual(len(addresses), 20)
+        addresses = yield account.change.get_addresses()
+        self.assertEqual(len(addresses), 6)
+
+    @defer.inlineCallbacks
     def test_generate_account_from_seed(self):
         account = Account.from_seed(
-            self.coin,
+            self.ledger,
             u"carbon smart garage balance margin twelve chest sword toast envelope bottom stomach ab"
             u"sent",
             u"lbryum"
         )
         self.assertEqual(
             account.private_key.extended_key_string(),
-            b'LprvXPsFZUGgrX1X9HiyxABZSf6hWJK7kHv4zGZRyyiHbBq5Wu94cE1DMvttnpLYReTPNW4eYwX9dWMvTz3PrB'
-            b'wwbRafEeA1ZXL69U2egM4QJdq'
+            b'xprv9s21ZrQH143K42ovpZygnjfHdAqSd9jo7zceDfPRogM7bkkoNVv7DRNLEoB8'
+            b'HoirMgH969NrgL8jNzLEegqFzPRWM37GXd4uE8uuRkx4LAe'
         )
         self.assertEqual(
             account.public_key.extended_key_string(),
-            b'Lpub2hkYkGHXktBhLpwUhKKogyuJ1M7Gt9EkjFTVKyDqZiZpWdhLuCoT1eKDfXfysMFfG4SzfXXcA2SsHzrjHK'
-            b'Ea5aoCNRBAhjT5NPLV6hXtvEi'
+            b'xpub661MyMwAqRbcGWtPvbWh9sc2BCfw2cTeVDYF23o3N1t6UZ5wv3EMmDgp66FxH'
+            b'uDtWdft3B5eL5xQtyzAtkdmhhC95gjRjLzSTdkho95asu9'
         )
-        self.assertEqual(
-            account.receiving_keys.generate_next_address(),
-            b'bCqJrLHdoiRqEZ1whFZ3WHNb33bP34SuGx'
-        )
-        private_key = account.get_private_key_for_address(b'bCqJrLHdoiRqEZ1whFZ3WHNb33bP34SuGx')
+        address = yield account.receiving.ensure_address_gap()
+        self.assertEqual(address[0], b'bCqJrLHdoiRqEZ1whFZ3WHNb33bP34SuGx')
+
+        private_key = yield self.ledger.get_private_key_for_address(b'bCqJrLHdoiRqEZ1whFZ3WHNb33bP34SuGx')
         self.assertEqual(
             private_key.extended_key_string(),
-            b'LprvXTnmVLXGKvRGo2ihBE6LJ771G3VVpAx2zhTJvjnx5P3h6iZ4VJX8PvwTcgzJZ1hqXX61Wpn4pQoP6n2wgp'
-            b'S8xjzCM6H2uGzCXuAMy5H9vtA'
+            b'xprv9vwXVierUTT4hmoe3dtTeBfbNv1ph2mm8RWXARU6HsZjBaAoFaS2FRQu4fptR'
+            b'AyJWhJW42dmsEaC1nKnVKKTMhq3TVEHsNj1ca3ciZMKktT'
         )
-        self.assertIsNone(account.get_private_key_for_address(b'BcQjRlhDOIrQez1WHfz3whnB33Bp34sUgX'))
+        private_key = yield self.ledger.get_private_key_for_address(b'BcQjRlhDOIrQez1WHfz3whnB33Bp34sUgX')
+        self.assertIsNone(private_key)
 
     def test_load_and_save_account(self):
         account_data = {
@@ -65,34 +71,17 @@ class TestAccount(unittest.TestCase):
                 "h absent",
             'encrypted': False,
             'private_key':
-                'LprvXPsFZUGgrX1X9HiyxABZSf6hWJK7kHv4zGZRyyiHbBq5Wu94cE1DMvttnpLYReTPNW4eYwX9dWMvTz3PrB'
-                'wwbRafEeA1ZXL69U2egM4QJdq',
+                'xprv9s21ZrQH143K42ovpZygnjfHdAqSd9jo7zceDfPRogM7bkkoNVv7DRNLEoB8'
+                'HoirMgH969NrgL8jNzLEegqFzPRWM37GXd4uE8uuRkx4LAe',
             'public_key':
-                'Lpub2hkYkGHXktBhLpwUhKKogyuJ1M7Gt9EkjFTVKyDqZiZpWdhLuCoT1eKDfXfysMFfG4SzfXXcA2SsHzrjHK'
-                'Ea5aoCNRBAhjT5NPLV6hXtvEi',
+                'xpub661MyMwAqRbcGWtPvbWh9sc2BCfw2cTeVDYF23o3N1t6UZ5wv3EMmDgp66FxH'
+                'uDtWdft3B5eL5xQtyzAtkdmhhC95gjRjLzSTdkho95asu9',
             'receiving_gap': 10,
-            'receiving_keys': [
-                '02c68e2d1cf85404c86244ffa279f4c5cd00331e996d30a86d6e46480e3a9220f4',
-                '03c5a997d0549875d23b8e4bbc7b4d316d962587483f3a2e62ddd90a21043c4941'
-            ],
+            'receiving_maximum_use_per_address': 2,
             'change_gap': 10,
-            'change_keys': [
-                '021460e8d728eee325d0d43128572b2e2bacdc027e420451df100cf9f2154ea5ab'
-            ]
+            'change_maximum_use_per_address': 2,
         }
 
-        account = Account.from_dict(self.coin, account_data)
-
-        self.assertEqual(len(account.receiving_keys.addresses), 2)
-        self.assertEqual(
-            account.receiving_keys.addresses[0],
-            b'bCqJrLHdoiRqEZ1whFZ3WHNb33bP34SuGx'
-        )
-        self.assertEqual(len(account.change_keys.addresses), 1)
-        self.assertEqual(
-            account.change_keys.addresses[0],
-            b'bFpHENtqugKKHDshKFq2Mnb59Y2bx4vKgL'
-        )
-
-        account_data['coin'] = 'lbc_mainnet'
+        account = Account.from_dict(self.ledger, account_data)
+        account_data['ledger'] = 'lbc_mainnet'
         self.assertDictEqual(account_data, account.to_dict())
