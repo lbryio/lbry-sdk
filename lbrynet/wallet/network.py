c06d91c0be317a2e4a3c5942a8e8d3ae8ac4f66f
Victor Shyba <victor1984@riseup.net>
2018-07-10 01:31:21 -0300

    adds get_claims_by_ids and fix a test name

--- lbrynet/wallet/network.py
+++ lbrynet/wallet/network.py
@@ -5,3 +5,6 @@ class Network(BaseNetwork):
 
     def get_values_for_uris(self, block_hash, *uris):
         return self.rpc('blockchain.claimtrie.getvaluesforuris', block_hash, *uris)
+
+    def get_claims_by_ids(self, *claim_ids):
+        return self.rpc("blockchain.claimtrie.getclaimsbyids", *claim_ids)
