444666481df37fa035ea48eea6419745edee00eb
Lex Berezhny <lex@damoti.com>
2018-07-21 14:02:35 -0400

    use twisted[tls] instead of specifying twisted dependencies in our own setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 # See https://packaging.python.org/requirements/  and
 # https://caremad.io/posts/2013/07/setup-vs-requirement/  for more details.
 requires = [
-    'Twisted',
+    'twisted[tls]',
     'appdirs',
     'distro',
     'base58',
@@ -33,9 +33,6 @@ requires = [
     'docopt',
     'colorama==0.3.7',
     'six',
-    'pyopenssl',
-    'service-identity',
-    'simplejson'
 ]
 
 console_scripts = [
