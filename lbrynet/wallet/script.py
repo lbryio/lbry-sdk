075e5350a35d4c2685bd227e12afb777e6ddf777
Lex Berezhny <lex@damoti.com>
2018-08-14 16:16:29 -0400

    Purchase Transaction Type

--- lbrynet/wallet/script.py
+++ lbrynet/wallet/script.py
@@ -12,6 +12,7 @@ class OutputScript(BaseOutputScript):
     OP_CLAIM_NAME = 0xb5
     OP_SUPPORT_CLAIM = 0xb6
     OP_UPDATE_CLAIM = 0xb7
+    OP_PURCHASE_CLAIM = 0xb8
 
     CLAIM_NAME_OPCODES = (
         OP_CLAIM_NAME, PUSH_SINGLE('claim_name'), PUSH_SINGLE('claim'),
@@ -46,13 +47,25 @@ class OutputScript(BaseOutputScript):
             UPDATE_CLAIM_OPCODES + BaseOutputScript.PAY_SCRIPT_HASH.opcodes
     ))
 
+    PURCHASE_CLAIM_OPCODES = (
+        OP_PURCHASE_CLAIM, PUSH_SINGLE('claim_id'), OP_2DROP
+    )
+    PURCHASE_CLAIM_PUBKEY = Template('purchase_claim+pay_pubkey_hash', (
+            PURCHASE_CLAIM_OPCODES + BaseOutputScript.PAY_PUBKEY_HASH.opcodes
+    ))
+    PURCHASE_CLAIM_SCRIPT = Template('purchase_claim+pay_script_hash', (
+            PURCHASE_CLAIM_OPCODES + BaseOutputScript.PAY_SCRIPT_HASH.opcodes
+    ))
+
     templates = BaseOutputScript.templates + [
         CLAIM_NAME_PUBKEY,
         CLAIM_NAME_SCRIPT,
         SUPPORT_CLAIM_PUBKEY,
         SUPPORT_CLAIM_SCRIPT,
         UPDATE_CLAIM_PUBKEY,
-        UPDATE_CLAIM_SCRIPT
+        UPDATE_CLAIM_SCRIPT,
+        PURCHASE_CLAIM_PUBKEY,
+        PURCHASE_CLAIM_SCRIPT
     ]
 
     @classmethod
@@ -60,6 +73,13 @@ class OutputScript(BaseOutputScript):
         return cls(template=cls.CLAIM_NAME_PUBKEY, values={
             'claim_name': claim_name,
             'claim': claim,
+            'pubkey_hash': pubkey_hash
+        })
+
+    @classmethod
+    def purchase_claim_pubkey_hash(cls, claim_id, pubkey_hash):
+        return cls(template=cls.PURCHASE_CLAIM_PUBKEY, values={
+            'claim_id': claim_id,
             'pubkey_hash': pubkey_hash
         })
 
@@ -85,5 +105,12 @@ class OutputScript(BaseOutputScript):
         return self.template.name.startswith('support_claim+')
 
     @property
+    def is_purchase_claim(self):
+        return self.template.name.startswith('purchase_claim+')
+
+    @property
     def is_claim_involved(self):
-        return self.is_claim_name or self.is_support_claim or self.is_update_claim
+        return any((
+            self.is_claim_name, self.is_support_claim,
+            self.is_update_claim, self.is_purchase_claim
+        ))
