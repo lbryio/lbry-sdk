f5625adcfc695f0fcbaeecbb2affc1bab70fc088
Victor Shyba <victor1984@riseup.net>
2018-08-15 19:24:23 -0300

    use int builtins instead of struct

--- lbrynet/dht/node.py
+++ lbrynet/dht/node.py
@@ -1,6 +1,5 @@
 import binascii
 import hashlib
-import struct
 import logging
 from functools import reduce
 
@@ -512,7 +511,7 @@ class Node(MockKademliaHelper):
         elif not self.verify_token(token, compact_ip):
             raise ValueError("Invalid token")
         if 0 <= port <= 65536:
-            compact_port = struct.pack('>H', port)
+            compact_port = port.to_bytes(2, byteorder='big', signed=False)
         else:
             raise TypeError('Invalid port: {}'.format(port))
         compact_address = compact_ip + compact_port + rpc_contact.id
@@ -577,7 +576,7 @@ class Node(MockKademliaHelper):
         # if we don't have k storing peers to return and we have this hash locally, include our contact information
         if len(peers) < constants.k and key in self._dataStore.completed_blobs:
             compact_ip = reduce(lambda buff, x: buff + bytearray([int(x)]), self.externalIP.split('.'), bytearray())
-            compact_port = struct.pack('>H', self.peerPort)
+            compact_port = self.peerPort.to_bytes(2, byteorder='big', signed=False)
             compact_address = compact_ip + compact_port + self.node_id
             peers.append(compact_address)
 
