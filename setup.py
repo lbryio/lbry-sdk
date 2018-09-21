fa0d13e630d092f26362d13ff5430b39eac28db7
Lex Berezhny <lex@damoti.com>
2018-09-20 20:40:30 -0400

    protobuf==3.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'envparse',
         'jsonrpc',
         'cryptography',
-        'protobuf==3.6.1',
+        'protobuf==3.2.0',
         'jsonschema',
         'ecdsa',
         'torba',
