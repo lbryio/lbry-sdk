65cfc1f63c099a381c115015d94ae61487a6bd9a
Lex Berezhny <lex@damoti.com>
2018-09-20 20:03:07 -0400

    removed lbryschema references from scripts and copied lbryschema requirements into local setup.py

--- scripts/wine_build.sh
+++ scripts/wine_build.sh
@@ -5,14 +5,12 @@ rm -rf /tmp/.wine-*
 apt-get -qq update
 apt-get -qq install -y git
 
-git clone https://github.com/lbryio/lbryschema.git --depth 1
 git clone https://github.com/lbryio/torba.git --depth 1
 git clone https://github.com/twisted/twisted.git --depth 1 --branch twisted-18.7.0
 sed -i -e '172,184{s/^/#/}' twisted/src/twisted/python/_setup.py
 
 pip install setuptools_scm
 cd twisted && pip install -e .[tls] && cd ..
-cd lbryschema && pip install -e . && cd ..
 cd torba && pip install -e . && cd ..
 
 cd lbry
