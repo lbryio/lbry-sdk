3364ac0976e48daedb40e0edbdc8232a0127c8ec
Jack Robison <jackrobison@lbry.io>
2018-10-18 18:22:46 -0400

    bump

--- lbrynet/__init__.py
+++ lbrynet/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 __name__ = "lbrynet"
-__version__ = "0.30.0rc8"
+__version__ = "0.30.0rc9"
 version = tuple(__version__.split('.'))
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
