1614a14b8c825ad9a28be332e72d552744f1827e
Jack Robison <jackrobison@lbry.io>
2019-01-03 19:09:40 -0500

    fix metadata encoding in file dictionary

--- lbrynet/stream/managed_stream.py
+++ lbrynet/stream/managed_stream.py
@@ -84,7 +84,7 @@ class ManagedStream:
 
     @property
     def metadata(self) ->typing.Optional[typing.Dict]:
-        return None if not self.stream_claim_info else self.stream_claim_info.claim['stream']['metadata']
+        return None if not self.stream_claim_info else self.stream_claim_info.claim.claim_dict['stream']['metadata']
 
     @property
     def blobs_completed(self) -> int:
