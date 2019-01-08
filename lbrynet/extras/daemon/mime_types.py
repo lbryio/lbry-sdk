6524e83008e109fb304bb351a4718204a51a2139
Alex Grintsvayg <git@grin.io>
2019-01-08 15:27:13 -0500

    if media type is not listed, default to a type that includes the file extension

--- lbrynet/extras/daemon/mime_types.py
+++ lbrynet/extras/daemon/mime_types.py
@@ -151,9 +151,6 @@ types_map = {
 }
 
 
-fallback_mime_type = 'application/octet-stream'
-
-
-def guess_mime_type(path):
+def guess_media_type(path):
     _, ext = os.path.splitext(path)
-    return types_map.get(ext, fallback_mime_type)
+    return types_map.get(ext, f'application/x-ext-{ext}')
