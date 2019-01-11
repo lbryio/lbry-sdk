351e7b2ad576b0e69cc59b0e232043010ca25e78
binaryflesh <logan.campos123@gmail.com>
2019-01-10 20:33:50 -0600

    Added base protocol factory

--- lbrynet/extras/reflector/common.py
+++ lbrynet/extras/reflector/common.py
@@ -1,3 +1,6 @@
+import asyncio
+from functools import wraps
+
 REFLECTOR_V1 = 0
 REFLECTOR_V2 = 1
 
@@ -25,3 +28,13 @@ class IncompleteResponse(Exception):
     Raised by reflector server when client sends a portion of a json request,
     used buffering the incoming request
     """
+
+
+def reflector_factory(task_factory):
+    @wraps(task_factory)
+    def base_protocol(loop, coro):
+        next_task = asyncio.tasks.Task(coro, loop=loop)
+        current_task = asyncio.Task.current_task(loop=loop)
+        previous_task = getattr(current_task, 'current_task', None)
+        setattr(next_task, 'current_task', previous_task)
+    return base_protocol
