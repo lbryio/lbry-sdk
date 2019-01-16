0c9b1fd4673c335787203b25a5ce4ecdf7ba368d
binaryflesh <logan.campos123@gmail.com>
2019-01-16 00:37:01 -0600

    updates

--- lbrynet/extras/reflector/reupload.py
+++ lbrynet/extras/reflector/reupload.py
@@ -2,54 +2,126 @@ import asyncio
 import typing
 import random
 import json
+import logging
+import re
 
 from lbrynet import conf
 
 if typing.TYPE_CHECKING:
     from lbrynet.blob.blob_manager import BlobFileManager
-    from lbrynet.blob.blob_file import BlobFile
     from lbrynet.stream.descriptor import StreamDescriptor
 
+log = logging.getLogger(__name__)
 
-class ReflectorClientProtocol(asyncio.Protocol):
-    def __init__(self, loop: asyncio.AbstractEventLoop):
-        self.__done = loop.create_future()
-        self.server_version_received = False
-        self.transport = None
-        self.blobs_reflected = []
-        loop.run_until_complete(self.__done)
-    
-    def connection_made(self, transport: asyncio.transports.Transport):
-        transport.write(json.dumps("{'version': 1,}").encode())
-        self.transport = BlobFile.open_for_writing(transport.get_extra_info('peerhost'))
-        transport.close()
-        
-    async def data_received(self, data: bytes):
-        if not self.server_version_received:
-            assert data.decode() == json.dumps("{'version': 1,}"), ConnectionRefusedError
-            self.server_version_received = True
-        else:
-            self.transport.write(data.decode)
-        async with self.server_version_received:
-            self.blobs_reflected.append(data)
+REFLECTOR_V1 = 0
+REFLECTOR_V2 = 1
+REFLECTOR_PROD_SERVER = random.choice(conf.settings['reflector_servers'])
 
-    def eof_received(self):
-        return self.blobs_reflected
-    
-    def connection_lost(self, exc: typing.Optional[Exception]):
-        self.__done.set_result(exc)
-    
 
-async def reflect_stream(descriptor: StreamDescriptor,
-                         blob_manager: BlobFileManager,
-                         reflector_url: typing.AnyStr) -> typing.List[str]:
+class ReflectorClientVersionError(Exception):
+    """
+    Raised by reflector server if client sends an incompatible or unknown version
+    """
+
+
+class ReflectorRequestError(Exception):
+    """
+    Raised by reflector server if client sends a message without the required fields
+    """
+
+
+class ReflectorRequestDecodeError(Exception):
+    """
+    Raised by reflector server if client sends an invalid json request
+    """
+
+
+class IncompleteResponse(Exception):
+    """
+    Raised by reflector server when client sends a portion of a json request,
+    used buffering the incoming request
+    """
+
+
+# TODO: verify if using binascii.hexlify() is supposed to be used instead.
+async def send_handshake(
+        protocol_version: typing.Optional[int],
+        reflector_server_url: typing.Optional[str]):
+    """
+    Reflector Handshake
     
-    assert reflector_url is not None, setattr(reflector_url, 'value', random.choice(conf.settings['reflector_servers']))
+    Simple Text-Oriented Messaging Protocol
+    to reliable determine if both the client
+    and server are ready to begin transaction.
+    
+    returns StreamReader and StreamWriter respectively
+    """
+    
+    loop = asyncio.get_running_loop()
+    received_server_version = loop.get_task_factory()
+    
+    client_version = protocol_version if not None else REFLECTOR_V2
+    if not isinstance(client_version, int):
+        try:
+            import urllib.parse
+            _version = urllib.parse.quote_from_bytes(client_version)
+            loop.call_soon_threadsafe(log.fatal, 'Got malformed bytes string: %s' % _version)
+        finally:
+            client_version = 1
+    
+    url = reflector_server_url if not None else REFLECTOR_PROD_SERVER
+    if not re.fullmatch(r"^[A-Za-z0-9._~()'!*:@,;+?-]*$", url):
+        import urllib.parse
+        _url = urllib.parse.quote_plus(url)
+        try:
+            loop.call_soon_threadsafe(log.fatal, 'Got malformed URI: %s' % _url)
+        finally:
+            url = urllib.parse.urlencode(_url)
+            
+    reader, writer = await asyncio.open_connection(host=url)
+    
+    try:
+        writer.write(json.dumps(
+            f"{'server': {client_version!r},}".strip()).encode('utf-8') + b'\n')
+        data = await reader.readline()
+        _temp = json.loads(data.decode('utf-8'))['version']
+        asyncio.run_coroutine_threadsafe(
+            received_server_version().set_result(_temp), loop)
+        loop.call_soon_threadsafe(log.info, '%s accepted connection.' % url)
+    except KeyError:
+        loop.call_soon_threadsafe(
+            log.error, '%s did not return the protocol version.' % url)
+        return asyncio.run_coroutine_threadsafe(
+            received_server_version().cancel(), loop)
+    except ValueError:
+        loop.call_soon_threadsafe(
+            log.error, '%s did not respond according to protocol specification.' % url)
+        return asyncio.run_coroutine_threadsafe(
+            received_server_version().cancel(), loop)
+    except ConnectionError:
+        loop.call_soon_threadsafe(
+            log.error, '%s connection terminated abruptly.' % url)
+        return asyncio.run_coroutine_threadsafe(
+            received_server_version().cancel(), loop)
+    finally:
+        await writer.drain()
+        if received_server_version().cancelled():
+            return None
+        loop.call_soon_threadsafe(
+            log.info, 'Server protocol version: %i' % received_server_version.result)
+        return reader, writer
+
+
+async def reflect_stream(descriptor: typing.Optional[StreamDescriptor],
+                         blob_manager: typing.Optional[BlobFileManager],
+                         reflector_server_url: typing.Optional[str]) -> typing.List[str]:
     loop = asyncio.get_event_loop()
-    blob_hashes = loop.create_future()
-    blob_hashes.set_result(loop.create_connection(host=reflector_url, protocol_factory=ReflectorClientProtocol))
-    # assert isinstance(blob_hashes.result, list), ConnectionError
-    return await blob_hashes.result()
+    handshake = asyncio.run_coroutine_threadsafe(
+        send_handshake(REFLECTOR_V2, REFLECTOR_PROD_SERVER), loop)
+    async with handshake.running():
+        # TODO: prepare all context variables.
+        _loop = asyncio.new_event_loop()
+    return []
 
 """
 Initiative lbry#1776:
