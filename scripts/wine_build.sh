946724e193ddb56156aeb5023d11328ce75b1959
Jack Robison <jackrobison@lbry.io>
2019-01-17 00:09:15 -0500

    update wine_build.sh

--- scripts/wine_build.sh
+++ scripts/wine_build.sh
@@ -6,11 +6,8 @@ apt-get -qq update
 apt-get -qq install -y git
 
 git clone https://github.com/lbryio/torba.git --depth 1
-git clone https://github.com/twisted/twisted.git --depth 1 --branch twisted-18.7.0
-sed -i -e '172,184{s/^/#/}' twisted/src/twisted/python/_setup.py
 
 pip install setuptools_scm
-cd twisted && pip install -e .[tls] && cd ..
 cd torba && pip install -e . && cd ..
 
 cd lbry
@@ -20,10 +17,6 @@ wget -Onetifaces-0.10.7-cp37-cp37m-win32.whl https://ci.appveyor.com/api/buildjo
 pip install netifaces-0.10.7-cp37-cp37m-win32.whl
 
 pip install -e .
-
-# Twisted needs that, but installing before lbry makes everything hang
-# PyInstaller removed that as a dependency in 3.4
-# The source file from Twisted that requires it is "src/twisted/internet/stdio.py"
 pip install pywin32
 
 pyinstaller --additional-hooks-dir=scripts/. --icon=icons/lbry256.ico -F -n lbrynet lbrynet/extras/cli.py
