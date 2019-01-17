f60999099383a9a1cdd902c8a314fd0a0ed7c3ff
binaryflesh <logan.campos123@gmail.com>
2019-01-17 03:32:28 -0600

    Merge branch 'async-protocols-refactor' into asyncio-reflector
    # Conflicts:
    #	lbrynet/blob/blob_file.py
    #	lbrynet/blob/blob_manager.py
    #	lbrynet/blob/writer.py
    #	lbrynet/blob_exchange/client.py
    #	lbrynet/blob_exchange/price_negotiation/offer.py
    #	lbrynet/blob_exchange/price_negotiation/payment_rate_manager.py
    #	lbrynet/blob_exchange/price_negotiation/price_model.py
    #	lbrynet/blob_exchange/price_negotiation/strategy.py
    #	lbrynet/blob_exchange/serialization.py
    #	lbrynet/conf.py
    #	lbrynet/dht/blob_announcer.py
    #	lbrynet/dht/constants.py
    #	lbrynet/dht/distance.py
    #	lbrynet/dht/node.py
    #	lbrynet/dht/protocol/data_store.py
    #	lbrynet/dht/protocol/distance.py
    #	lbrynet/dht/protocol/protocol.py
    #	lbrynet/dht/routing/distance.py
    #	lbrynet/dht/routing/kbucket.py
    #	lbrynet/dht/serialization/bencoding.py
    #	lbrynet/dht/serialization/datagram.py
    #	lbrynet/extras/cli.py
    #	lbrynet/extras/daemon/ComponentManager.py
    #	lbrynet/extras/daemon/Components.py
    #	lbrynet/extras/daemon/Daemon.py
    #	lbrynet/extras/daemon/analytics.py
    #	lbrynet/extras/daemon/exchange_rate_manager.py
    #	lbrynet/extras/daemon/migrator/migrate8to9.py
    #	lbrynet/extras/reflector/reupload.py
    #	lbrynet/storage.py
    #	lbrynet/stream/assembler.py
    #	lbrynet/stream/descriptor.py
    #	lbrynet/stream/downloader.py
    #	lbrynet/stream/managed_stream.py
    #	lbrynet/stream/stream_manager.py
    #	scripts/time_to_first_byte.py
    #	tests/unit/blob_exchange/test_transfer_blob.py
    #	tests/unit/dht/test_async_gen_junction.py
    #	tests/unit/stream/test_assembler.py
    #	tests/unit/stream/test_downloader.py

--- lbrynet/extras/reflector/reupload.py
+++ lbrynet/extras/reflector/reupload.py
@@ -3,13 +3,17 @@ import binascii
 import json
 import random
 import typing
-
 from lbrynet import conf
 
-if typing.TYPE_CHECKING:
-    from lbrynet.blob.blob_manager import BlobFileManager
-    from lbrynet.stream.descriptor import StreamDescriptor
 
+if typing.TYPE_CHECKING:
+    from lbrynet.stream.assembler import StreamAssembler
+    from lbrynet.blob.blob_file import BlobFile
+    from lbrynet.blob.blob_manager import BlobFileManager
+    from lbrynet.stream.managed_stream import ManagedStream
+    from lbrynet.stream.descriptor import StreamDescriptor
+    from lbrynet.dht.protocol.data_store import DictDataStore
+    from lbrynet.dht.peer import PeerManager
 
 class ReflectorClientVersionError(Exception):
     """
@@ -36,100 +40,74 @@ class IncompleteResponse(Exception):
     """
 
 
+REFLECTOR_V1 = 0
+REFLECTOR_V2 = 1
+PROD_SERVER = random.choice(conf.settings['reflector_servers'])
+
+SEND_SD_BLOB = 'send_sd_blob'
+NEEDED_SD_BLOBS = 'needed_sd_blobs'
+
+
 class ReflectorClient(asyncio.Protocol):
     
-    REFLECTOR_V1 = 0
-    REFLECTOR_V2 = 1
-    PROD_SERVER = random.choice(conf.settings['reflector_servers'])
+    def __init__(self, loop: typing.Optional[asyncio.BaseEventLoop],
+                 blob_manager: typing.Optional[BlobFileManager],
+                 descriptor: typing.Optional[StreamDescriptor],
+                 version: typing.Optional[int], url: typing.Optional[str],
+                 server: NotImplemented) -> None:
+        server |= url
+        loop |= loop | asyncio.get_event_loop_policy().get_event_loop()
+        version |= version | REFLECTOR_V2 | REFLECTOR_V1
+        self.descriptor = StreamDescriptor(..., ..., ..., ..., ..., ...)
+        self.peer_manager = PeerManager(loop)
+        self.dict_data = DictDataStore(loop, self.peer_manager)
+        self.blob_manager = BlobFileManager(loop, ..., self.dict_data)
+        self.blob_file = BlobFileManager(loop, ..., self.dict_data)
+        self.managed_stream = ManagedStream(loop, blob_manager, descriptor, ..., ...)
+        self.stream_descriptor = StreamAssembler(loop, blob_manager, descriptor.calculate_sd_hash())
+        super(ReflectorClient, self).__init__()
     
-    def __init__(self, version: int, server_url: str):
-        self.version = version
-        self.server = server_url
-        self.server_version = self.handle_handshake()
-        self.descriptor = asyncio.Event()
-        self.blob_file_manager = asyncio.Event()
-        
-    async def handle_handshake(self) -> typing.Any[int, Exception]:
-        """
-        Handshake sequence.
-        """
-        reader, writer = await asyncio.open_connection(host=self.server)
-        handshake = {'version': self.version}
+    async def connection_made(self, transport: asyncio.Transport) -> None:
+        reader, writer = transport
+        handshake = {'version': REFLECTOR_V2}
         payload = binascii.hexlify(json.dumps(handshake).encode()).decode()
-        await writer.write(payload)
-        await writer.write_eof()
-        data = await reader.readline()
-        response = await json.loads(binascii.unhexlify(data))
+        writer.write(payload)
+        writer.write_eof()
+        data = reader.readline()
+        response = json.loads(binascii.unhexlify(data))
         await writer.drain()
-        return await response.result()['version']
+        return response.result()['version'] == 0 | 1
+        
+    async def data_received(self, data: bytes) -> None:
+        _msg = await json.loads((binascii.unhexlify(data)))
+        key, value = _msg
+        sd_hash = self.descriptor.calculate_sd_hash()
+        if key.value == SEND_SD_BLOB and value:
+            sd, _ = BlobFileManager(asyncio.get_running_loop(), ...,  ...).get_stream_descriptor(sd_hash)
+            BlobFileManager.check_completed_blobs(sd, ...)
+            await BlobFile(sd, ..., ...).sendfile(sd)
+        elif key is NEEDED_SD_BLOBS or value:
+            sd_blobs, _condition = value
+            if _condition:
+                self.blob_manager.get_stream_descriptor(sd_hash)
+                self.dict_data.completed_blobs.update(sd_blobs)
 
-    """
-    ############# Stream descriptor requests and responses #############
-    (if sending blobs directly this is skipped)
-    If the client is reflecting a whole stream, they send a stream descriptor request:
-    {
-        'sd_blob_hash': str,
-        'sd_blob_size': int
-    }
-
-    The server indicates if it's aware of this stream already by requesting (or not requesting)
-    the stream descriptor blob. If the server has a validated copy of the sd blob, it will
-    include the needed_blobs field (a list of blob hashes missing from reflector) in the response.
-    If the server does not have the sd blob the needed_blobs field will not be included, as the
-    server does not know what blobs it is missing - so the client should send all of the blobs
-    in the stream.
-    {
-        'send_sd_blob': bool
-        'needed_blobs': list, conditional
-    }
-
-    The client may begin the file transfer of the sd blob if send_sd_blob was True.
-    If the client sends the blob, after receiving it the server indicates if the
-    transfer was successful:
-    {
-        'received_sd_blob': bool
-    }
-    If the transfer was not successful (False), the blob is added to the needed_blobs queue
-    """
-
-    # Expecting:
-    # {
-    #     'sd_blob_hash': str,
-    #     'sd_blob_size': int
-    # }
-    async def reflect_stream(self, descriptor: typing.Optional[StreamDescriptor],
-                 blob_manager: typing.Optional[BlobFileManager]) -> typing.List[str]:
+    async def eof_received(self):
         ...
     
-    """
-    ############# Blob requests and responses #############
-    A client with blobs to reflect (either populated by the client or by the stream descriptor
-    response) queries if the server is ready to begin transferring a blob
-    {
-        'blob_hash': str,
-        'blob_size': int
-    }
+    async def pause_writing(self):
+        ...
+    
+    async def resume_writing(self):
+        ...
 
-    The server replies, send_blob will be False if the server has a validated copy of the blob:
-    {
-        'send_blob': bool
-    }
 
-    The client may begin the raw blob file transfer if the server replied True.
-    If the client sends the blob, the server replies:
-    {
-        'received_blob': bool
-    }
-    If the transfer was not successful (False), the blob is re-added to the needed_blobs queue
-
-    Blob requests continue for each of the blobs the client has queued to send, when completed
-    the client disconnects.
-    """
-
-    # hotfix for lbry#1776
-    # TODO: ReflectorClient choreography
-    # TODO: return ok | error to daemon
-    # TODO: Unit test to verify blob handling is solid
-    # TODO: mitmproxy transaction for potential constraints to watch for
-    # TODO: Unit test rewrite for lbrynet.extras.daemon.file_reflect use case
-    # TODO: squash previous commits
+async def reflect_stream(loop: typing.Optional[asyncio.get_running_loop()],
+                         descriptor: typing.Optional[StreamDescriptor],
+                         blob_manager: typing.Optional[BlobFileManager],
+                         version: int, url: str, server: None) -> typing.List[str]:
+    
+    _list = typing.cast(list, asyncio.run_coroutine_threadsafe(
+        ReflectorClient(loop, blob_manager, descriptor, REFLECTOR_V1, PROD_SERVER, None), asyncio.get_event_loop()))
+    
+    return _list
