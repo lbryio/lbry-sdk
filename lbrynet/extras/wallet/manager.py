368f0683ec0f36e9644e9153aa6fd077a0efb2be
Victor Shyba <victor1984@riseup.net>
2019-01-22 12:24:42 -0300

    fix claim_list for channels

--- lbrynet/extras/wallet/manager.py
+++ lbrynet/extras/wallet/manager.py
@@ -281,8 +281,9 @@ class LbryWalletManager(BaseWalletManager):
     async def get_claims_for_name(self, name: str):
         response = await self.ledger.network.get_claims_for_name(name)
         if 'claims' in response:
-            to_resolve = [(claim['name'] + '#' + claim['claim_id']) for claim in response['claims']]
-            response['claims'] = [resolution['claim'] for resolution in (await self.resolve(*to_resolve)).values()]
+            resolutions = await self.resolve(*[f"{claim['name']}#{claim['claim_id']}" for claim in response['claims']])
+            resolutions = resolutions.values()
+            response['claims'] = [resolution.get('claim', resolution.get('certificate')) for resolution in resolutions]
         return response
 
     async def address_is_mine(self, unknown_address, account):
