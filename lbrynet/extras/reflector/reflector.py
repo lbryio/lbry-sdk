2dca0a46b7abd7772967576102b7a2a6eb0f2de0
binaryflesh <logan.campos123@gmail.com>
2019-01-25 16:09:25 -0600

    docstrings

--- lbrynet/extras/reflector/reflector.py
+++ lbrynet/extras/reflector/reflector.py
@@ -12,38 +12,103 @@ __all__ = ('reflect', 'Reflector')
 
 
 class _Reflector(typing.Type):
-    __doc__ = 'Reflector Module constants'
+    """
+    Reflector Module constants
+    """
     V2 = 1
     VERSION = typing.Any[V2]
-    SERVERS = 'reflector.lbry.io'  # conf.get_config()['reflector_servers']
+    SERVERS = 'reflector.lbry.io'
     HOST = random.choice(SERVERS)
     PORT = 5566
 
 
 class ReflectorClientVersionError(Exception):
-    __doc__ = 'Raised by reflector server if client sends an incompatible or unknown version.'
+    """
+    Raised by reflector server if client sends an incompatible or unknown version.
+    """
     __cause__ = _Reflector.VERSION is not _Reflector.V2
     __context__ = ValueError
 
 
 class ReflectorRequestError(Exception):
-    __doc__ = 'Raised by reflector server if client sends a message without the required fields.'
+    """
+    Raised by reflector server if client sends a message without the required fields.
+    """
     __context__ = BlockingIOError
 
 
 class ReflectorDecodeError(Exception):
-    __doc__ = 'Raised by reflector server if client sends an invalid json request.'
+    """
+    Raised by reflector server if client sends an invalid json request.
+    """
     __context__ = json.JSONDecodeError
 
 
 class IncompleteResponse(Exception):
-    __doc__ = 'Raised by reflector server when client sends a portion of a json request, ' \
-              'used buffering the incoming request.'
+    """
+    Raised by reflector server when client sends a portion of a json request,
+    used buffering the incoming request.
+    """
     __context__ = BufferError
 
 
 class Reflector(asyncio.Protocol):
-    __doc__ = 'Reflector Protocol: re-uploads a stream to a reflector server'
+    """
+    Reflector is a protocol to re-host lbry blobs and streams
+    Client queries and server responses follow, all dicts are encoded as json
+    ############# Handshake request and response #############
+    Upon connecting, the client sends a version handshake:
+    {
+        'version': int,
+    }
+    The server replies with the same version
+    {
+        'version': int,
+    }
+    ############# Stream descriptor requests and responses #############
+    (if sending blobs directly this is skipped)
+    If the client is reflecting a whole stream, they send a stream descriptor request:
+    {
+        'sd_blob_hash': str,
+        'sd_blob_size': int
+    }
+    The server indicates if it's aware of this stream already by requesting (or not requesting)
+    the stream descriptor blob. If the server has a validated copy of the sd blob, it will
+    include the needed_blobs field (a list of blob hashes missing from reflector) in the response.
+    If the server does not have the sd blob the needed_blobs field will not be included, as the
+    server does not know what blobs it is missing - so the client should send all of the blobs
+    in the stream.
+    {
+        'send_sd_blob': bool
+        'needed_blobs': list, conditional
+    }
+    The client may begin the file transfer of the sd blob if send_sd_blob was True.
+    If the client sends the blob, after receiving it the server indicates if the
+    transfer was successful:
+    {
+        'received_sd_blob': bool
+    }
+    If the transfer was not successful (False), the blob is added to the needed_blobs queue
+    ############# Blob requests and responses #############
+    A client with blobs to reflect (either populated by the client or by the stream descriptor
+    response) queries if the server is ready to begin transferring a blob
+    {
+        'blob_hash': str,
+        'blob_size': int
+    }
+    The server replies, send_blob will be False if the server has a validated copy of the blob:
+    {
+        'send_blob': bool
+    }
+    The client may begin the raw blob file transfer if the server replied True.
+    If the client sends the blob, the server replies:
+    {
+        'received_blob': bool
+    }
+    If the transfer was not successful (False), the blob is re-added to the needed_blobs queue
+    Blob requests continue for each of the blobs the client has queued to send, when completed
+    the client disconnects.
+    """
     __metaclass__ = typing.Protocol
 
     def __init__(self, storage, descriptor, reflector_server, reflector_port):
@@ -61,9 +126,8 @@ class Reflector(asyncio.Protocol):
     async def _decode(cls, message: bytes) -> typing.Dict:
         return await json.loads(binascii.unhexlify(message.decode()).encode())
 
-    def connection_made(self, transport: asyncio.Transport):
+    def connection_made(self, transport: asyncio.Transport) -> asyncio.Transport:
         self.transport = transport
-        # print(f'connected to {writer.get_extra_info("peerhost")}')
         _handshake = self._encode({'version': _Reflector.V2})
         self.transport.write(await _handshake)
         return self.transport
@@ -89,10 +153,10 @@ class Reflector(asyncio.Protocol):
         return await self.storage.get_blobs_for_stream(self._send_sd_blob())
 
 
-async def reflect(storage: SQLiteStorage, *,
-                  descriptor: StreamDescriptor = None,
-                  reflector_server: typing.AnyStr = 'reflector.lbry.io',
-                  reflector_port: int = 5566) -> typing.Any[typing.List]:
+async def reflect(
+        storage: SQLiteStorage, *, descriptor: StreamDescriptor = None,
+        reflector_server: str = 'reflector.lbry.io',
+        reflector_port: int = 5566) -> typing.Any[typing.Awaitable[typing.List]]:
     """
     Reflect Blobs to Reflector
     Usage:
