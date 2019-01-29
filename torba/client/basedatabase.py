45669dad336f7b74dd7c04d3f826620ed2f777c7
Victor Shyba <victor1984@riseup.net>
2019-01-29 14:51:59 -0300

    simplify add_keys

--- torba/client/basedatabase.py
+++ torba/client/basedatabase.py
@@ -489,12 +489,12 @@ class BaseDatabase(SQLiteMixin):
             return addresses[0]
 
     async def add_keys(self, account, chain, keys):
-        sql = "insert into pubkey_address (address, account, chain, position, pubkey) values (?, ?, ?, ?, ?)"
-        values = (
-            (pubkey.address, account.public_key.address, chain, position, sqlite3.Binary(pubkey.pubkey_bytes))
-            for position, pubkey in keys
+        await self.db.executemany(
+            "insert into pubkey_address (address, account, chain, position, pubkey) values (?, ?, ?, ?, ?)",
+            ((pubkey.address, account.public_key.address, chain,
+              position, sqlite3.Binary(pubkey.pubkey_bytes))
+             for position, pubkey in keys)
         )
-        await self.db.executemany(sql, values)
 
     async def _set_address_history(self, address, history):
         await self.db.execute(
