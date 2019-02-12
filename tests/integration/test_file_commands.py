11c86a7143659f9517769136504d71ab93483e7f
Victor Shyba <victor1984@riseup.net>
2019-02-12 20:42:01 -0300

    add integration test for different download errors

--- tests/integration/test_file_commands.py
+++ tests/integration/test_file_commands.py
@@ -21,3 +21,20 @@ class FileCommands(CommandTestCase):
 
         await self.daemon.jsonrpc_get('lbry://foo')
         self.assertEqual(len(self.daemon.jsonrpc_file_list()), 1)
+
+    async def test_download_different_timeouts(self):
+        claim = await self.make_claim('foo', '0.01')
+        sd_hash = claim['output']['value']['stream']['source']['source']
+        await self.daemon.jsonrpc_file_delete(claim_name='foo')
+        all_except_sd = [
+            blob_hash for blob_hash in self.server.blob_manager.completed_blob_hashes if blob_hash != sd_hash
+        ]
+        await self.server.blob_manager.delete_blobs(all_except_sd)
+        resp = await self.daemon.jsonrpc_get('lbry://foo', timeout=2)
+        self.assertIn('error', resp)
+        self.assertEquals('Failed to download data blobs for sd hash %s within timeout' % sd_hash, resp['error'])
+        await self.daemon.jsonrpc_file_delete(claim_name='foo')
+        await self.server.blob_manager.delete_blob(sd_hash)
+        resp = await self.daemon.jsonrpc_get('lbry://foo', timeout=2)
+        self.assertIn('error', resp)
+        self.assertEquals('Failed to download sd blob %s within timeout' % sd_hash, resp['error'])
