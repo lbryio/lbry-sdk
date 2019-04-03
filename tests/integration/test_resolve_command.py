3b1c37298dc50425948d5762b72252dcb4c7b223
hackrush <hackrush@lbry.io>
2019-04-03 14:55:43 +0530

    Added integration tests

--- tests/integration/test_resolve_command.py
+++ tests/integration/test_resolve_command.py
@@ -90,33 +90,65 @@ class ResolveCommand(CommandTestCase):
         self.assertEqual(claim['effective_amount'], "0.1")
 
     async def test_canonical_resolve_url(self):
+        # test replicating https://github.com/lbryio/lbry/issues/958#issue-266120456
         kauffj1, kauffj2, grin, gorn1, gorn2, gorn3 = await self.get_account_ids()
 
-        # get canonical url for @kauffj
-        channel_k1 = await self.make_channel('@kauffj', '0.01', account_id=kauffj1)
-        claim_id1 = channel_k1['claim_id']
-        # canonical_url_k1 = (await self.resolve('lbry://@kauffj'))['lbry://@kauffj']['certificate']['canonical_url']
+        # create 3 channels
+        channel_k1 = await self.make_channel('@kauffj', '0.00001', account_id=kauffj1)
+        claim_id_k1 = channel_k1['claim_id']
+        await self.assertCorrectCanonicalUrl('@kauffj', channel_k1['claim_id'])
 
-        # check if resolution is proper
-        resolve1 = await self.resolve("lbry://@kauffj#{}".format(claim_id1[0]))
-        resolve2 = await self.resolve("lbry://@kauffj")
-        x = resolve1["lbry://@kauffj#{}".format(claim_id1[0])]
-        y = resolve2["lbry://@kauffj"]
-        self.assertResolveDictEqual(x, y)
+        channel_k2 = await self.get_colliding_claim(None, '@kauffj', claim_id_k1, 1, kauffj2, 'channel')
+        await self.assertCorrectCanonicalUrl('@kauffj', channel_k2['claim_id'])
 
-        claim_g1 = await self.make_claim('gornado', '0.02', account_id=gorn1)
-        claim_id2 = claim_g1['claim_id']
-        await self.make_claim('gornado', '0.01', channel_name='@kauffj', account_id=kauffj1)
-        z = await self.resolve("lbry://gornado")
-        w = await self.resolve("lbry://@kauffj/gornado")
-        qq = w["lbry://@kauffj/gornado"]
+        channel_g = await self.make_channel('@grin', "0.00001", account_id=grin)
+        await self.assertCorrectCanonicalUrl('@grin', channel_g['claim_id'])
+
+        # create and test claims
+        claim_g1 = await self.make_claim('gornado', '0.00001', account_id=gorn1)
+        claim_id_g1 = claim_g1['claim_id']
+        await self.assertCorrectCanonicalUrl('gornado', claim_id_g1)
+
+        claim_g2 = await self.get_colliding_claim('gornado', None, claim_id_g1, 1, gorn1, 'claim')
+        await self.assertCorrectCanonicalUrl('gornado', claim_g2['claim_id'])
+
+        claim_gk1 = await self.make_claim('gornado', '0.00001', channel_name='@kauffj', account_id=kauffj1)
+        await self.assertCorrectCanonicalUrl('gornado', claim_gk1['claim_id'])
+
+        claim_gk2 = await self.make_claim('gornado', '0.00001', channel_name='@kauffj', account_id=kauffj2)
+        await self.assertCorrectCanonicalUrl('gornado', claim_gk2['claim_id'])
+
+        claim_gg = await self.make_claim('gornado', '0.00001', channel_name='@grin', account_id=grin)
+        await self.assertCorrectCanonicalUrl('gornado', claim_gg['claim_id'])
+
+    async def assertCorrectCanonicalUrl(self, name, claim_id):
+        full_url = "{}#{}".format(name, claim_id)
+        resolve_with_claim_id = (await self.resolve(full_url))[full_url]
+        result = resolve_with_claim_id.get('claim', resolve_with_claim_id.get('certificate'))
+        canonical_url = result.get('canonical_url')
+
+        resolve_with_canonical_url = (await self.resolve(canonical_url))[canonical_url]
+
+        self.assertResolveDictEqual(
+            resolve_with_claim_id,
+            resolve_with_canonical_url
+        )
 
     def assertResolveDictEqual(self, resolve_dict1, resolve_dict2):
-        if resolve_dict1['certificate'].get('valid_at_height'):
-            resolve_dict1['certificate'].pop('valid_at_height')
+        keys = ['certificate', 'claim']
+        for key in resolve_dict1:
+            if key in keys:
+                if 'valid_at_height' in resolve_dict1[key]:
+                    resolve_dict1[key].pop('valid_at_height')
+                if 'absolute_channel_position' in resolve_dict1[key]:
+                    resolve_dict1[key].pop('absolute_channel_position')
 
-        if resolve_dict2['certificate'].get('valid_at_height'):
-            resolve_dict2['certificate'].pop('valid_at_height')
+        for key in resolve_dict2:
+            if key in keys:
+                if 'valid_at_height' in resolve_dict2[key]:
+                    resolve_dict2[key].pop('valid_at_height')
+                if 'absolute_channel_position' in resolve_dict2[key]:
+                    resolve_dict2[key].pop('absolute_channel_position')
 
         self.assertDictEqual(resolve_dict1, resolve_dict2)
 
@@ -124,11 +156,23 @@ class ResolveCommand(CommandTestCase):
         account_ids = list()
         for i in range(1, 7):
             account_id = (await self.daemon.jsonrpc_account_create('account_{}'.format(i)))['id']
-            account_address = await self.daemon.jsonrpc_address_unused(account_id)
-            result = await self.out(self.daemon.jsonrpc_wallet_send('1.0', account_address))
-            await self.confirm_tx(result['txid'])
+            tx = await self.daemon.jsonrpc_account_fund(account_id, None, '1.0', outputs=1000, broadcast=True)
+            await self.confirm_tx(tx.id)
             account_ids.append(account_id)
 
         await self.generate(5)
-
         return account_ids
+
+    async def get_colliding_claim(self, name, channel_name, claim_id, length, account_id, claim_type):
+        while True:
+            if claim_type == "channel":
+                claim = await self.make_channel(channel_name, "0.00001", True, account_id)
+            else:
+                claim = await self.make_claim(
+                    name, "0.00001", channel_name=channel_name, confirm=True, account_id=account_id
+                )
+
+            if claim.get('claim_id').startswith(claim_id[:length]):
+                break
+
+        return claim
