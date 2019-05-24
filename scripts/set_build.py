f7bde2365fb2fa1978c50f1391a743b4229cc90f
Jack Robison <jackrobison@lbry.io>
2019-05-24 17:32:33 -0400

    improve set_build.py, pin torba version
    - adds BUILD_COMMIT to lbrynet.build_type

--- scripts/set_build.py
+++ scripts/set_build.py
@@ -1,33 +1,35 @@
+"""Set the build version to be 'qa', 'rc', 'release'"""
 
-"""Set the build version to be 'dev', 'qa', 'rc', 'release'"""
-
-import os.path
-import re
-import subprocess
 import sys
+import os
+import re
+import logging
+
+log = logging.getLogger()
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+
+
+def get_build_type(travis_tag=None):
+    if not travis_tag:
+        return "qa"
+    log.debug("getting build type for tag: \"%s\"", travis_tag)
+    if re.match('v\d+\.\d+\.\d+rc\d+$', travis_tag):
+        return 'rc'
+    elif re.match('v\d+\.\d+\.\d+$', travis_tag):
+        return 'release'
+    return 'qa'
 
 
 def main():
-    build = get_build()
     root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    with open(os.path.join(root_dir, 'lbrynet', 'build_type.py'), 'w') as f:
-        f.write("BUILD = '{}'\n".format(build))
-
-
-def get_build():
-    try:
-        tag = subprocess.check_output(['git', 'describe', '--exact-match', '--all']).strip()
-        if re.match('tags\/v\d+\.\d+\.\d+rc\d+$', tag.decode()):
-            print('Build: rc')
-            return 'rc'
-        elif re.match('tags\/v\d+\.\d+\.\d+$', tag.decode()):
-            print('Build: release')
-            return 'release'
-        print('Build: qa')
-        return 'qa'
-    except subprocess.CalledProcessError:
-        print("Couldn't determine build type, defaulting to qa.")
-        return 'qa'
+    build_type_path = os.path.join(root_dir, 'lbrynet', 'build_type.py')
+    log.debug("configuring build type file: %s", build_type_path)
+    travis_commit = os.environ['TRAVIS_COMMIT'][:6]
+    build_type = get_build_type(os.environ.get('TRAVIS_TAG', None))
+    log.debug("setting build type=%s, build commit=%s", build_type, travis_commit)
+    with open(build_type_path, 'w') as f:
+        f.write("BUILD = \"{}\"\nBUILD_COMMIT = \"{}\"\n".format(build_type, travis_commit))
 
 
 if __name__ == '__main__':
