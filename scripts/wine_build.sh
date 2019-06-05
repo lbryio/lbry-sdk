98f8dc7bf410fd2b33656ad2bd6e029e81538d3b
Jack Robison <jackrobison@lbry.io>
2019-06-05 17:17:04 -0400

    fix torba version on travis

--- scripts/wine_build.sh
+++ scripts/wine_build.sh
@@ -1,8 +1,11 @@
 set -x
+# must also be updated in travis.yml
+TORBA_VERSION=v0.5.3
 rm -rf /tmp/.wine-*
 
 apt-get -qq update
 apt-get -qq install -y git
+
 pip install setuptools_scm
 
 cd lbry
@@ -11,7 +14,13 @@ cd lbry
 wget -Onetifaces-0.10.7-cp37-cp37m-win32.whl https://ci.appveyor.com/api/buildjobs/6hworunifsymrhp2/artifacts/dist%2Fnetifaces-0.10.7-cp37-cp37m-win32.whl
 pip install netifaces-0.10.7-cp37-cp37m-win32.whl
 
-pip install git+https://github.com/lbryio/torba.git@${1}#egg=torba
+git clone --depth=1 --single-branch --branch ${TORBA_VERSION} https://github.com/lbryio/torba.git
+cd torba
+pip install .
+cd ..
+rm -rf torba
+
+pip show torba
 pip install -e .
 pip install pywin32
 
