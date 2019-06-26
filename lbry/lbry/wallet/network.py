dcb1e53abac2df57c5bfb1e43b309935d91e44ac
Victor Shyba <victor1984@riseup.net>
2019-06-26 03:42:11 -0300

    remove unused network command

--- lbry/lbry/wallet/network.py
+++ lbry/lbry/wallet/network.py
@@ -3,9 +3,6 @@ from torba.client.basenetwork import BaseNetwork
 
 class Network(BaseNetwork):
 
-    def get_server_height(self):
-        return self.rpc('blockchain.block.get_server_height', [])
-
     def get_claims_by_ids(self, claim_ids):
         return self.rpc('blockchain.claimtrie.getclaimsbyids', claim_ids)
 
