a0d13f2172da6ba2a8c7272861010edf105d4c43
Lex Berezhny <lex@damoti.com>
2019-07-13 00:32:09 -0400

    added TRACK_METRICS env variable to torba wallet server

--- torba/torba/server/env.py
+++ torba/torba/server/env.py
@@ -38,6 +38,7 @@ class Env:
         self.db_dir = self.required('DB_DIRECTORY')
         self.db_engine = self.default('DB_ENGINE', 'leveldb')
         self.max_query_workers = self.integer('MAX_QUERY_WORKERS', None)
+        self.track_metrics = self.boolean('TRACK_METRICS', False)
         self.websocket_host = self.default('WEBSOCKET_HOST', None)
         self.websocket_port = self.integer('WEBSOCKET_PORT', None)
         self.daemon_url = self.required('DAEMON_URL')
