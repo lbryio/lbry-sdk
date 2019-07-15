30f21b89fa2b6f0fa36965d08358563e15145dd0
Lex Berezhny <lex@damoti.com>
2019-07-15 01:12:14 -0400

    refactor

--- dart/packages/lbry/lib/src/server_connection.dart
+++ dart/packages/lbry/lib/src/server_connection.dart
@@ -5,36 +5,54 @@ import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
 
-class Client {
-    String url;
+class ServerConnection {
+    String url = "";
     IOWebSocketChannel channel;
-    StreamController<MetricDataPoint> _metricsController = StreamController.broadcast();
-    Stream<MetricDataPoint> get metrics => _metricsController.stream;
+    bool get isConnected => channel != null && channel.closeCode == null;
 
-    Client(this.url);
+    final StreamController<ServerLoadDataPoint> _loadDataController = StreamController.broadcast();
+    Stream<ServerLoadDataPoint> get load_data => _loadDataController.stream;
+
+    ServerConnection({this.url});
 
     open() {
+        if (isConnected) return Future.value('already open');
         channel = IOWebSocketChannel.connect(this.url);
         int tick = 1;
         channel.stream.listen((message) {
             Map data = json.decode(message);
-            Map commands = data['commands'];
-            _metricsController.add(
-                MetricDataPoint(
+            print(data);
+            Map commands = data['commands'] ?? {};
+            _loadDataController.add(
+                ServerLoadDataPoint(
                     tick,
-                    CommandMetrics.from_map(commands['search'] ?? {}),
-                    CommandMetrics.from_map(commands['resolve'] ?? {})
+                    APICallMetrics.from_map(commands['search'] ?? {}),
+                    APICallMetrics.from_map(commands['resolve'] ?? {})
                 )
             );
             tick++;
         });
     }
 
-    Future close() => channel.sink.close(status.goingAway);
+    close() {
+        if (isConnected) {
+            return channel.sink.close(status.goingAway);
+        }
+        return Future.value('already closed');
+    }
+
+    subscribe_to_server_load_data() {
+        if (isConnected) channel.sink.add('subscribe');
+    }
+
+    unsubscribe_from_server_load_data() {
+        if (isConnected) channel.sink.add('unsubscribe');
+    }
+
 }
 
 
-class CommandMetrics {
+class APICallMetrics {
     final int started;
     final int finished;
     final int total_time;
@@ -46,7 +64,7 @@ class CommandMetrics {
     final int avg_execution_time;
     final int avg_query_time_per_search;
     final int avg_query_time_per_query;
-    CommandMetrics(
+    APICallMetrics(
         this.started, this.finished, this.total_time,
         this.execution_time, this.query_time, this.query_count):
         avg_wait_time=finished > 0 ? ((total_time - (execution_time + query_time))/finished).round() : 0,
@@ -54,7 +72,7 @@ class CommandMetrics {
         avg_execution_time=finished > 0 ? (execution_time/finished).round() : 0,
         avg_query_time_per_search=finished > 0 ? (query_time/finished).round() : 0,
         avg_query_time_per_query=query_count > 0 ? (query_time/query_count).round() : 0;
-    CommandMetrics.from_map(Map data): this(
+    APICallMetrics.from_map(Map data): this(
         data['started'] ?? 0,
         data['finished'] ?? 0,
         data['total_time'] ?? 0,
@@ -65,20 +83,20 @@ class CommandMetrics {
 }
 
 
-class MetricDataPoint {
+class ServerLoadDataPoint {
     final int tick;
-    final CommandMetrics search;
-    final CommandMetrics resolve;
-    MetricDataPoint(this.tick, this.search, this.resolve);
-    MetricDataPoint.empty():
+    final APICallMetrics search;
+    final APICallMetrics resolve;
+    ServerLoadDataPoint(this.tick, this.search, this.resolve);
+    ServerLoadDataPoint.empty():
         tick = 0,
-        search=CommandMetrics.from_map({}),
-        resolve=CommandMetrics.from_map({});
+        search=APICallMetrics.from_map({}),
+        resolve=APICallMetrics.from_map({});
 }
 
 
-cli() {
-    Client('ws://localhost:8181/')..open()..metrics.listen((m) {
+connect_and_listen_for_load_data() {
+    ServerConnection(url: 'ws://localhost:8181/')..open()..load_data.listen((m) {
         print(m.search);
     });
 }
