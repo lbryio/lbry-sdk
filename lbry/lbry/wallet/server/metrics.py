08da763327ee9426f3e4313724c02f35215cd407
Lex Berezhny <lex@damoti.com>
2019-07-18 19:15:01 -0400

    more metrics cleanup

--- lbry/lbry/wallet/server/metrics.py
+++ lbry/lbry/wallet/server/metrics.py
@@ -7,12 +7,13 @@ def calculate_elapsed(start) -> int:
     return int((time.perf_counter() - start) * 1000)
 
 
-def calculate_percentiles(data) -> Tuple[int, int, int, int, int, int, int]:
+def calculate_avg_percentiles(data) -> Tuple[int, int, int, int, int, int, int, int]:
     if not data:
-        return 0, 0, 0, 0, 0, 0, 0
+        return 0, 0, 0, 0, 0, 0, 0, 0
     data.sort()
     size = len(data)
     return (
+        int(sum(data) / size),
         data[0],
         data[math.ceil(size * .05) - 1],
         data[math.ceil(size * .25) - 1],
@@ -23,10 +24,6 @@ def calculate_percentiles(data) -> Tuple[int, int, int, int, int, int, int]:
     )
 
 
-def avg(data) -> int:
-    return int(sum(data) / len(data)) if data else 0
-
-
 def remove_select_list(sql) -> str:
     return sql[sql.index('FROM'):]
 
@@ -35,79 +32,87 @@ class APICallMetrics:
 
     def __init__(self, name):
         self.name = name
-        # total counts
-        self.cache_hits = 0
-        self.started = 0
-        self.errored = 0
+
+        # total requests received
+        self.receive_count = 0
+        self.cache_response_count = 0
+
+        # millisecond timings for query based responses
+        self.query_response_times = []
+        self.query_intrp_times = []
+        self.query_error_times = []
+
+        self.query_python_times = []
+        self.query_wait_times = []
+        self.query_sql_times = []  # aggregate total of multiple SQL calls made per request
+
+        self.individual_sql_times = []  # every SQL query run on server
+
+        # actual queries
         self.errored_queries = set()
-        self.interrupted = 0
         self.interrupted_queries = set()
-        # timings
-        self.command_total_times = []
-        self.command_query_times = []
-        self.command_execution_times = []
-        self.command_wait_times = []
-        self.individual_query_times = []
 
     def to_json_and_reset(self):
         return {
-            # total counts
-            "cache_hits_count": self.cache_hits,
-            "started_count": self.started,
-            "finished_count": len(self.command_total_times),
-            "errored_count": self.errored,
+            # total requests received
+            "receive_count": self.receive_count,
+            # sum of these is total responses made
+            "cache_response_count": self.cache_response_count,
+            "query_response_count": len(self.query_response_times),
+            "intrp_response_count": len(self.query_intrp_times),
+            "error_response_count": len(self.query_error_times),
+            # millisecond timings for non-cache responses
+            "response": calculate_avg_percentiles(self.query_response_times),
+            "interrupt": calculate_avg_percentiles(self.query_intrp_times),
+            "error": calculate_avg_percentiles(self.query_error_times),
+            # response, interrupt and error each also report the python, wait and sql stats:
+            "python": calculate_avg_percentiles(self.query_python_times),
+            "wait": calculate_avg_percentiles(self.query_wait_times),
+            "sql": calculate_avg_percentiles(self.query_sql_times),
+            # extended timings for individual sql executions
+            "individual_sql": calculate_avg_percentiles(self.individual_sql_times),
+            "individual_sql_count": len(self.individual_sql_times),
+            # actual queries
             "errored_queries": list(self.errored_queries),
-            "interrupted_count": self.interrupted,
             "interrupted_queries": list(self.interrupted_queries),
-            "individual_queries_count": len(self.individual_query_times),
-            # timings and percentiles
-            "total_avg": avg(self.command_total_times),
-            "total_percentiles": calculate_percentiles(self.command_total_times),
-            "query_avg": avg(self.command_query_times),
-            "query_percentiles": calculate_percentiles(self.command_query_times),
-            "execution_avg": avg(self.command_execution_times),
-            "execution_percentiles": calculate_percentiles(self.command_execution_times),
-            "wait_avg": avg(self.command_wait_times),
-            "wait_percentiles": calculate_percentiles(self.command_wait_times),
-            "individual_query_avg": avg(self.individual_query_times),
-            "individual_query_percentiles": calculate_percentiles(self.individual_query_times),
         }
 
-    def cache_hit(self):
-        self.cache_hits += 1
-
     def start(self):
-        self.started += 1
+        self.receive_count += 1
 
-    def finish(self, start, metrics):
-        self.command_total_times.append(calculate_elapsed(start))
+    def cache_response(self):
+        self.cache_response_count += 1
+
+    def _add_query_timings(self, request_total_time, metrics):
         if metrics and 'execute_query' in metrics:
-            query_times = [f['total'] for f in metrics['execute_query']]
-            self.individual_query_times.extend(query_times)
-            command_query_time = sum(query_times)
-            self.command_query_times.append(command_query_time)
-            self.command_execution_times.append(
-                metrics[self.name][0]['total'] - command_query_time
-            )
-            self.command_wait_times.append(
-                self.command_total_times[-1] - metrics[self.name][0]['total']
-            )
+            sub_process_total = metrics[self.name][0]['total']
+            individual_query_times = [f['total'] for f in metrics['execute_query']]
+            aggregated_query_time = sum(individual_query_times)
+            self.individual_sql_times.extend(individual_query_times)
+            self.query_sql_times.append(aggregated_query_time)
+            self.query_python_times.append(sub_process_total - aggregated_query_time)
+            self.query_wait_times.append(request_total_time - sub_process_total)
 
-    def _add_queries(self, metrics, query_set):
+    @staticmethod
+    def _add_queries(query_set, metrics):
         if metrics and 'execute_query' in metrics:
             for execute_query in metrics['execute_query']:
                 if 'sql' in execute_query:
                     query_set.add(remove_select_list(execute_query['sql']))
 
-    def interrupt(self, start, metrics):
-        self.finish(start, metrics)
-        self._add_queries(metrics, self.interrupted_queries)
+    def query_response(self, start, metrics):
+        self.query_response_times.append(calculate_elapsed(start))
+        self._add_query_timings(self.query_response_times[-1], metrics)
 
-    def error(self, start, metrics=None):
-        self.errored += 1
-        if metrics:
-            self.finish(start, metrics)
-            self._add_queries(metrics, self.errored_queries)
+    def query_interrupt(self, start, metrics):
+        self.query_intrp_times.append(calculate_elapsed(start))
+        self._add_queries(self.interrupted_queries, metrics)
+        self._add_query_timings(self.query_intrp_times[-1], metrics)
+
+    def query_error(self, start, metrics):
+        self.query_error_times.append(calculate_elapsed(start))
+        self._add_queries(self.errored_queries, metrics)
+        self._add_query_timings(self.query_error_times[-1], metrics)
 
 
 class ServerLoadData:
