650d8a44d76a785dedc941dc08c72c087688e3dd
Victor Shyba <victor1984@riseup.net>
2019-07-19 20:03:18 -0300

    change picking logic to probe the session before considering it a good session

--- torba/tests/client_tests/integration/test_network.py
+++ torba/tests/client_tests/integration/test_network.py
@@ -53,35 +53,45 @@ class ReconnectTests(IntegrationTestCase):
 
 
 class ServerPickingTestCase(AsyncioTestCase):
-    async def _make_fake_server(self, latency=1.0, port=1337):
-        # local fake server with artificial latency
-        proto = RPCSession()
-        async def __handler(_):
-            await asyncio.sleep(latency)
-            return {'height': 1}
 
-        proto.handle_request = __handler
-        server = await self.loop.create_server(lambda: proto, host='127.0.0.1', port=port)
+    async def _make_fake_server(self, latency=1.0, port=1):
+        # local fake server with artificial latency
+        class FakeSession(RPCSession):
+            async def handle_request(self, request):
+                await asyncio.sleep(latency)
+                return {"height": 1}
+        server = await self.loop.create_server(lambda: FakeSession(), host='127.0.0.1', port=port)
+        self.addCleanup(server.close)
+        return '127.0.0.1', port
+
+    async def _make_bad_server(self, port=42420):
+        async def echo(reader, writer):
+            while True:
+                writer.write(await reader.read())
+        server = await asyncio.start_server(echo, host='127.0.0.1', port=port)
         self.addCleanup(server.close)
         return '127.0.0.1', port
 
     async def test_pick_fastest(self):
         ledger = Mock(config={
             'default_servers': [
-                await self._make_fake_server(latency=1.5, port=1340),
-                await self._make_fake_server(latency=0.1, port=1337),
-                await self._make_fake_server(latency=1.0, port=1339),
-                await self._make_fake_server(latency=0.5, port=1338),
+                # fast but unhealthy, should be discarded
+                await self._make_bad_server(),
+                ('localhost', 1),
+                ('example.that.doesnt.resolve', 9000),
+                await self._make_fake_server(latency=1.2, port=1340),
+                await self._make_fake_server(latency=0.5, port=1337),
+                await self._make_fake_server(latency=0.7, port=1339),
             ],
-            'connect_timeout': 30
+            'connect_timeout': 3
         })
 
         network = BaseNetwork(ledger)
         self.addCleanup(network.stop)
         asyncio.ensure_future(network.start())
-        await asyncio.wait_for(network.on_connected.first, timeout=1)
+        await asyncio.wait_for(network.on_connected.first, timeout=3)
         self.assertTrue(network.is_connected)
         self.assertEqual(network.client.server, ('127.0.0.1', 1337))
         # ensure we are connected to all of them
-        self.assertEqual(len(network.session_pool.sessions), 4)
         self.assertTrue(all([not session.is_closing() for session in network.session_pool.sessions]))
+        self.assertEqual(len(network.session_pool.sessions), 3)
