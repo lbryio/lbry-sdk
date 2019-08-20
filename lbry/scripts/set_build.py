d7b316dfa70f323ab812bbc622c7b29f15f53c37
Alex Grintsvayg <git@grin.io>
2019-08-20 15:11:52 -0400

    gitlab ci setup
    what works:
    - tests
    - linux and mac builds
    - uploading builds to s3
    
    what's left to do:
    - uploading coverage results to codecov.io
    - windows build
    - github releases
    - what directories to cache between builds

--- lbry/scripts/set_build.py
+++ lbry/scripts/set_build.py
@@ -10,13 +10,13 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
 
 
-def get_build_type(travis_tag=None):
-    if not travis_tag:
+def get_build_type(ci_tag=None):
+    if not ci_tag:
         return "qa"
-    log.debug("getting build type for tag: \"%s\"", travis_tag)
-    if re.match(r'v\d+\.\d+\.\d+rc\d+$', travis_tag):
+    log.debug("getting build type for tag: \"%s\"", ci_tag)
+    if re.match(r'v\d+\.\d+\.\d+rc\d+$', ci_tag):
         return 'rc'
-    elif re.match(r'v\d+\.\d+\.\d+$', travis_tag):
+    elif re.match(r'v\d+\.\d+\.\d+$', ci_tag):
         return 'release'
     return 'qa'
 
@@ -25,11 +25,11 @@ def main():
     root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     build_type_path = os.path.join(root_dir, 'lbry', 'build_type.py')
     log.debug("configuring build type file: %s", build_type_path)
-    travis_commit = os.environ['TRAVIS_COMMIT'][:6]
-    build_type = get_build_type(os.environ.get('TRAVIS_TAG', None))
-    log.debug("setting build type=%s, build commit=%s", build_type, travis_commit)
+    commit_hash = os.getenv('CI_COMMIT_SHA', os.getenv('TRAVIS_COMMIT', None))[:6]
+    build_type = get_build_type(os.getenv('CI_COMMIT_TAG', os.getenv('TRAVIS_TAG', None)))
+    log.debug("setting build type=%s, build commit=%s", build_type, commit_hash)
     with open(build_type_path, 'w') as f:
-        f.write(f"BUILD = \"{build_type}\"\nBUILD_COMMIT = \"{travis_commit}\"\n")
+        f.write(f"BUILD = \"{build_type}\"\nBUILD_COMMIT = \"{commit_hash}\"\n")
 
 
 if __name__ == '__main__':
