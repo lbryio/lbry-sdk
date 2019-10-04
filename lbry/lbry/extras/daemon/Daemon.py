8fedf685eb5f8f31c6cbde9e4dd5df1fd27c3d37
Lex Berezhny <lex@damoti.com>
2019-10-03 23:14:09 -0400

    updating/abanding streams/channels works across all accounts

--- lbry/lbry/extras/daemon/Daemon.py
+++ lbry/lbry/extras/daemon/Daemon.py
@@ -2248,7 +2248,7 @@ class Daemon(metaclass=JSONRPCServerType):
             --clear_locations              : (bool) clear existing locations (prior to adding new ones)
             --thumbnail_url=<thumbnail_url>: (str) thumbnail url
             --cover_url=<cover_url>        : (str) url of cover image
-            --account_id=<account_id>      : (str) account to use for holding the transaction
+            --account_id=<account_id>      : (str) account in which to look for channel (default: all)
             --wallet_id=<wallet_id>        : (str) restrict operation to specific wallet
           --funding_account_ids=<funding_account_ids>: (list) ids of accounts to fund this transaction
             --claim_address=<claim_address>: (str) address where the channel is sent
@@ -2263,15 +2263,21 @@ class Daemon(metaclass=JSONRPCServerType):
         Returns: {Transaction}
         """
         wallet = self.wallet_manager.get_wallet_or_default(wallet_id)
-        account = wallet.get_account_or_default(account_id)
         funding_accounts = wallet.get_accounts_or_all(funding_account_ids)
+        if account_id:
+            account = wallet.get_account_or_error(account_id)
+            accounts = [account]
+        else:
+            account = wallet.default_account
+            accounts = wallet.accounts
 
         existing_channels = await self.ledger.get_claims(
-            wallet=wallet, accounts=[account] if account_id else wallet.accounts, claim_id=claim_id
+            wallet=wallet, accounts=accounts, claim_id=claim_id
         )
         if len(existing_channels) != 1:
+            account_ids = ', '.join(f"'{account.id}'" for account in accounts)
             raise Exception(
-                f"Can't find the channel '{claim_id}' in account '{account.id}'."
+                f"Can't find the channel '{claim_id}' in account(s) {account_ids}."
             )
         old_txo = existing_channels[0]
         if not old_txo.claim.is_channel:
@@ -2346,12 +2352,21 @@ class Daemon(metaclass=JSONRPCServerType):
         Returns: {Transaction}
         """
         wallet = self.wallet_manager.get_wallet_or_default(wallet_id)
-        account = wallet.get_account_or_default(account_id)
+        if account_id:
+            account = wallet.get_account_or_error(account_id)
+            accounts = [account]
+        else:
+            account = wallet.default_account
+            accounts = wallet.accounts
 
         if txid is not None and nout is not None:
-            claims = await account.get_claims(**{'txo.txid': txid, 'txo.position': nout})
+            claims = await self.ledger.get_claims(
+                wallet=wallet, accounts=accounts, **{'txo.txid': txid, 'txo.position': nout}
+            )
         elif claim_id is not None:
-            claims = await account.get_claims(claim_id=claim_id)
+            claims = await self.ledger.get_claims(
+                wallet=wallet, accounts=accounts, claim_id=claim_id
+            )
         else:
             raise Exception('Must specify claim_id, or txid and nout')
 
@@ -2854,7 +2869,7 @@ class Daemon(metaclass=JSONRPCServerType):
             --clear_channel                : (bool) remove channel signature
           --channel_account_id=<channel_account_id>: (str) one or more account ids for accounts to look in
                                                    for channel certificates, defaults to all accounts.
-            --account_id=<account_id>      : (str) account to use for holding the transaction
+            --account_id=<account_id>      : (str) account in which to look for stream (default: all)
             --wallet_id=<wallet_id>        : (str) restrict operation to specific wallet
           --funding_account_ids=<funding_account_ids>: (list) ids of accounts to fund this transaction
             --claim_address=<claim_address>: (str) address where the claim is sent to, if not specified
@@ -2869,13 +2884,21 @@ class Daemon(metaclass=JSONRPCServerType):
         Returns: {Transaction}
         """
         wallet = self.wallet_manager.get_wallet_or_default(wallet_id)
-        account = wallet.get_account_or_default(account_id)
         funding_accounts = wallet.get_accounts_or_all(funding_account_ids)
+        if account_id:
+            account = wallet.get_account_or_error(account_id)
+            accounts = [account]
+        else:
+            account = wallet.default_account
+            accounts = wallet.accounts
 
-        existing_claims = await account.get_claims(claim_id=claim_id)
+        existing_claims = await self.ledger.get_claims(
+            wallet=wallet, accounts=accounts, claim_id=claim_id
+        )
         if len(existing_claims) != 1:
+            account_ids = ', '.join(f"'{account.id}'" for account in accounts)
             raise Exception(
-                f"Can't find the claim '{claim_id}' in account '{account.id}'."
+                f"Can't find the stream '{claim_id}' in account(s) {account_ids}."
             )
         old_txo = existing_claims[0]
         if not old_txo.claim.is_stream:
@@ -2979,12 +3002,21 @@ class Daemon(metaclass=JSONRPCServerType):
         Returns: {Transaction}
         """
         wallet = self.wallet_manager.get_wallet_or_default(wallet_id)
-        account = wallet.get_account_or_default(account_id)
+        if account_id:
+            account = wallet.get_account_or_error(account_id)
+            accounts = [account]
+        else:
+            account = wallet.default_account
+            accounts = wallet.accounts
 
         if txid is not None and nout is not None:
-            claims = await account.get_claims(**{'txo.txid': txid, 'txo.position': nout})
+            claims = await self.ledger.get_claims(
+                wallet=wallet, accounts=accounts, **{'txo.txid': txid, 'txo.position': nout}
+            )
         elif claim_id is not None:
-            claims = await account.get_claims(claim_id=claim_id)
+            claims = await self.ledger.get_claims(
+                wallet=wallet, accounts=accounts, claim_id=claim_id
+            )
         else:
             raise Exception('Must specify claim_id, or txid and nout')
 
@@ -2992,14 +3024,14 @@ class Daemon(metaclass=JSONRPCServerType):
             raise Exception('No claim found for the specified claim_id or txid:nout')
 
         tx = await Transaction.create(
-            [Input.spend(txo) for txo in claims], [], [account], account
+            [Input.spend(txo) for txo in claims], [], accounts, account
         )
 
         if not preview:
             await self.broadcast_or_release(tx, blocking)
             await self.analytics_manager.send_claim_action('abandon')
         else:
-            await account.ledger.release_tx(tx)
+            await self.ledger.release_tx(tx)
 
         return tx
 
