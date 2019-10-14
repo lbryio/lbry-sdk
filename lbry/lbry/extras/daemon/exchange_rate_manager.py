1b2be5384e82c1e773bc22f8f46bba36586015cd
Miroslav Kovar <miroslavkovar@protonmail.com>
2019-10-14 11:17:18 +0200

    Refactor exchange rate manager

--- lbry/lbry/extras/daemon/exchange_rate_manager.py
+++ lbry/lbry/extras/daemon/exchange_rate_manager.py
@@ -4,6 +4,7 @@ import logging
 import json
 from decimal import Decimal
 from typing import Optional
+from json.decoder import JSONDecodeError
 from aiohttp.client_exceptions import ClientError
 from lbry.error import InvalidExchangeRateResponseError, CurrencyConversionError
 from lbry.utils import aiohttp_request
@@ -34,17 +35,59 @@ class ExchangeRate:
         return {'spot': self.spot, 'ts': self.ts}
 
 
-class MarketFeed:
-    REQUESTS_TIMEOUT = 20
-    EXCHANGE_RATE_UPDATE_RATE_SEC = 300
+class Deserializer:
+    def __init__(self, content_type):
+        self.content_type = content_type
+        self._deserializer = self._get_deserializer(content_type)
 
-    def __init__(self, market: str, name: str, url: str, params, fee):
-        self.market = market
-        self.name = name
+    def deserialize(self, body):
+        return self._deserializer(body)
+
+    def _get_deserializer(self, content_type):
+        if content_type == 'json':
+            return self._deserialize_json
+        else:
+            raise ValueError('Content type {content_type} is not supported')
+
+    def _deserialize_json(self, body):
+        try:
+            return json.loads(body)
+        except (ValueError, JSONDecodeError):
+            log.error('Failed to deserialize response body: %s', body)
+            raise
+
+
+class AioHttpManager:
+    REQUESTS_TIMEOUT = 20
+
+    def __init__(self, url, params, content_type):
         self.url = url
         self.params = params
+        self.content_type = content_type
+
+    async def _make_request(self):
+        async with aiohttp_request('get', self.url, params=self.params) as response:
+            return await response
+
+    async def get_response_body(self):
+        response = await asyncio.wait_for(self._make_request(), self.REQUESTS_TIMEOUT)
+        if self.content_type not in response.headers.get('Content-Type'):
+            raise InvalidExchangeRateResponse(self.url, f'Received response is not of type {self.content_type}')
+        return response.read().decode()
+
+
+class MarketFeed:
+    EXCHANGE_RATE_UPDATE_RATE_SEC = 300
+
+    def __init__(self, market: str, name: str, url: str, params: dict, fee: float,
+                 content_type: str = 'json', network_manager=AioHttpManager,
+                 deserializer=Deserializer):
+        self.market = market
+        self.name = name
         self.fee = fee
         self.rate = None
+        self._network_manager = network_manager(url, params, content_type)
+        self._deserializer = deserializer(content_type)
         self._task: Optional[asyncio.Task] = None
         self._online = True
 
@@ -54,11 +97,13 @@ class MarketFeed:
     def is_online(self):
         return self._online
 
-    async def _make_request(self) -> str:
-        async with aiohttp_request('get', self.url, params=self.params) as response:
-            return (await response.read()).decode()
+    def _on_error(self, err):
+        log.warning("There was a problem updating %s exchange rate information from %s",
+                    self.market, self.name)
+        log.debug("Exchange rate error (%s from %s): %s", self.market, self.name, err)
+        self._online = False
 
-    def _handle_response(self, response):
+    def _handle_response(self, body):
         raise NotImplementedError()
 
     def _subtract_fee(self, from_amount):
@@ -70,24 +115,24 @@ class MarketFeed:
         self.rate = ExchangeRate(self.market, price, int(time.time()))
         self._online = True
 
-    def _on_error(self, err):
-        log.warning("There was a problem updating %s exchange rate information from %s",
-                    self.market, self.name)
-        log.debug("Exchange rate error (%s from %s): %s", self.market, self.name, err)
-        self._online = False
+    async def _get_current_price(self):
+        body = self._deserializer.deserialize(await self._network_manager.get_response_body())
+        return self._subtract_fee(self._handle_response(body))
 
     async def _update_price(self):
+        try:
+            self._save_price(await self._get_current_price())
+        except (asyncio.TimeoutError, InvalidExchangeRateResponseError, ClientError) as err:
+            self._on_error(err)
+
+    async def _keep_updated(self):
         while True:
-            try:
-                response = await asyncio.wait_for(self._make_request(), self.REQUESTS_TIMEOUT)
-                self._save_price(self._subtract_fee(self._handle_response(response)))
-            except (asyncio.TimeoutError, InvalidExchangeRateResponseError, ClientError) as err:
-                self._on_error(err)
+            self._update_price()
             await asyncio.sleep(self.EXCHANGE_RATE_UPDATE_RATE_SEC)
 
     def start(self):
         if not self._task:
-            self._task = asyncio.create_task(self._update_price())
+            self._task = asyncio.create_task(self._keep_updated)
 
     def stop(self):
         if self._task and not self._task.done():
@@ -105,8 +150,7 @@ class BittrexFeed(MarketFeed):
             BITTREX_FEE
         )
 
-    def _handle_response(self, response):
-        json_response = json.loads(response)
+    def _handle_response(self, json_response):
         if 'result' not in json_response:
             raise InvalidExchangeRateResponseError(self.name, 'result not found')
         trades = json_response['result']
@@ -130,8 +174,7 @@ class LBRYioFeed(MarketFeed):
             0.0,
         )
 
-    def _handle_response(self, response):
-        json_response = json.loads(response)
+    def _handle_response(self, json_response):
         if 'data' not in json_response:
             raise InvalidExchangeRateResponseError(self.name, 'result not found')
         return 1.0 / json_response['data']['lbc_btc']
@@ -147,11 +190,7 @@ class LBRYioBTCFeed(MarketFeed):
             0.0,
         )
 
-    def _handle_response(self, response):
-        try:
-            json_response = json.loads(response)
-        except ValueError:
-            raise InvalidExchangeRateResponseError(self.name, "invalid rate response : %s" % response)
+    def _handle_response(self, json_response):
         if 'data' not in json_response:
             raise InvalidExchangeRateResponseError(self.name, 'result not found')
         return 1.0 / json_response['data']['btc_usd']
@@ -167,11 +206,7 @@ class CryptonatorBTCFeed(MarketFeed):
             0.0,
         )
 
-    def _handle_response(self, response):
-        try:
-            json_response = json.loads(response)
-        except ValueError:
-            raise InvalidExchangeRateResponseError(self.name, "invalid rate response")
+    def _handle_response(self, json_response):
         if 'ticker' not in json_response or len(json_response['ticker']) == 0 or \
                 'success' not in json_response or json_response['success'] is not True:
             raise InvalidExchangeRateResponseError(self.name, 'result not found')
@@ -188,11 +223,7 @@ class CryptonatorFeed(MarketFeed):
             0.0,
         )
 
-    def _handle_response(self, response):
-        try:
-            json_response = json.loads(response)
-        except ValueError:
-            raise InvalidExchangeRateResponseError(self.name, "invalid rate response")
+    def _handle_response(self, json_response):
         if 'ticker' not in json_response or len(json_response['ticker']) == 0 or \
                 'success' not in json_response or json_response['success'] is not True:
             raise InvalidExchangeRateResponseError(self.name, 'result not found')
