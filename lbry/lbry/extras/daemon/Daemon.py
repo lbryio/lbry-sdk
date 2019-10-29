f5b884867b4bd3854321e7159d21d4a245c9655c
Lex Berezhny <lex@damoti.com>
2019-10-29 08:58:55 -0400

    pylint fixes

--- lbry/lbry/extras/daemon/Daemon.py
+++ lbry/lbry/extras/daemon/Daemon.py
@@ -658,7 +658,7 @@ class Daemon(metaclass=JSONRPCServerType):
         Resolve a name and return the estimated stream cost
         """
 
-        resolved = await self.resolve(uri)
+        resolved = await self.resolve([], uri)
         if resolved:
             claim_response = resolved[uri]
         else:
@@ -3722,7 +3722,7 @@ class Daemon(metaclass=JSONRPCServerType):
 
         if uri or stream_hash or sd_hash:
             if uri:
-                metadata = (await self.resolve(uri))[uri]
+                metadata = (await self.resolve([], uri))[uri]
                 sd_hash = utils.get_sd_hash(metadata)
                 stream_hash = await self.storage.get_stream_hash_for_sd_hash(sd_hash)
             elif stream_hash:
@@ -3961,7 +3961,7 @@ class Daemon(metaclass=JSONRPCServerType):
             channel_url = comment.get('channel_url')
             if not channel_url:
                 continue
-            resolve_response = await self.resolve([channel_url])
+            resolve_response = await self.resolve([], [channel_url])
             if isinstance(resolve_response[channel_url], Output):
                 comment['is_channel_signature_valid'] = comment_client.is_comment_signed_by_channel(
                     comment, resolve_response[channel_url]
