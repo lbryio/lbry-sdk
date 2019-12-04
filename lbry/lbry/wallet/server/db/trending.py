38a22cc44150a63c22b64ad13d5c55c8e6c766aa
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-12-04 13:59:56 +1300

    trending update

--- lbry/lbry/wallet/server/db/trending.py
+++ lbry/lbry/wallet/server/db/trending.py
@@ -1,4 +1,115 @@
 from math import sqrt
+import time
+
+CREATE_TREND_TABLE = """
+    create table if not exists trend (
+        claim_hash bytes not null,
+        height integer not null,
+        amount integer not null,
+        primary key (claim_hash, height)
+    ) without rowid;
+"""
+
+
+# Half life in blocks
+half_life = 576
+
+# Decay coefficient
+decay = 0.5**(1.0/half_life)
+
+
+def soften(delta):
+    """
+    Softening function applies to LBC changes.
+    """
+    if delta >= 0.0:
+        return delta**0.3
+
+    # If delta is negative
+    mag = abs(delta)
+    return -(mag**0.3)
+
+
+class TrendingData:
+    """
+    An object of this class holds trending data
+    """
+    def __init__(self):
+
+        # Dict from claim_id to [total_amount, trending_score]
+        self.claims = {}
+        self.initialised = False
+
+
+    def insert_claim(self, claim_id, total_amount, trending_score):
+        """
+        Insert a claim (happens upon re-running wallet server to
+        repopulate trending data from the DB)
+        """
+        if claim_id in self.claims:
+            return
+
+        self.claims[claim_id] = [total_amount, trending_score]
+
+
+    def update_claim(self, claim_id, total_amount):
+        """
+        Update trending data for a claim, given its new total amount.
+        """
+
+        # Extract existing total amount and trending score
+        if claim_id in self.claims:
+            old_data = self.claims[claim_id]
+        else:
+            old_data = [0.0, 0.0]
+
+        trending_score = decay*old_data[1] \
+                            + soften(1E-8*(total_amount - old_data[0]))
+        self.claims[claim_id] = [total_amount, trending_score]
+
+
+# One global instance
+trending_data = TrendingData()
+
+
+def calculate_trending(db, height, final_height):
+
+    start = time.time()
+
+    # I'm using the original column names
+    # trending_global = my trending score
+    # trending_local  = old total amount
+    print("Calculating AR trending at block {h}...".format(h=height),
+                end="", flush=True)
+
+
+    # Read all values from db to re-init trending_data
+    if not trending_data.initialised:
+        for row in db.execute("""
+                          SELECT claim_id, amount, support_amount, trending_global
+                          FROM claim;
+                          """):
+            trending_data.insert_claim(row[0], row[1] + row[2], row[3])  
+        trending_data.initialised = True
+
+    # Update all claims from db
+    for row in db.execute("""
+                          SELECT claim_id, amount, support_amount
+                          FROM claim;
+                          """):
+        trending_data.update_claim(row[0], row[1] + row[2])
+
+    # Write trending scores to DB
+    if height % 20 == 0: # and height >= (final_height - 100):
+        the_list = []
+        keys = trending_data.claims.keys()
+        for key in keys:
+            the_list.append((trending_data.claims[key][1], key))
+        db.executemany("UPDATE claim SET trending_global=? WHERE claim_id=?;",
+                        the_list)
+
+    print("done. Took {time} seconds.".format(time=time.time() - start))
+
 
 # TRENDING_WINDOW is the number of blocks in ~6hr period (21600 seconds / 161 seconds per block)
 TRENDING_WINDOW = 134
@@ -50,69 +161,3 @@ class ZScore:
 
 def register_trending_functions(connection):
     connection.create_aggregate("zscore", 1, ZScore)
-
-
-def calculate_trending(db, height, final_height):
-    # don't start tracking until we're at the end of initial sync
-    if height < (final_height - (TRENDING_WINDOW * TRENDING_DATA_POINTS)):
-        return
-
-    if height % TRENDING_WINDOW != 0:
-        return
-
-    db.execute(f"""
-    DELETE FROM trend WHERE height < {height - (TRENDING_WINDOW * TRENDING_DATA_POINTS)}
-    """)
-
-    start = (height - TRENDING_WINDOW) + 1
-    db.execute(f"""
-    INSERT OR IGNORE INTO trend (claim_hash, height, amount)
-    SELECT claim_hash, {start}, COALESCE(
-            (SELECT SUM(amount) FROM support WHERE claim_hash=claim.claim_hash
-             AND height >= {start}), 0
-        ) AS support_sum
-    FROM claim WHERE support_sum > 0
-    """)
-
-    zscore = ZScore()
-    for (global_sum,) in db.execute("SELECT AVG(amount) FROM trend GROUP BY height"):
-        zscore.step(global_sum)
-    global_mean, global_deviation = 0, 1
-    if zscore.count > 0:
-        global_mean = zscore.mean
-        global_deviation = zscore.standard_deviation
-
-    db.execute(f"""
-    UPDATE claim SET
-        trending_local = COALESCE((
-            SELECT zscore(amount) FROM trend
-            WHERE claim_hash=claim.claim_hash ORDER BY height DESC
-        ), 0),
-        trending_global = COALESCE((
-            SELECT (amount - {global_mean}) / {global_deviation} FROM trend
-            WHERE claim_hash=claim.claim_hash AND height = {start}
-        ), 0),
-        trending_group = 0,
-        trending_mixed = 0
-    """)
-
-    # trending_group and trending_mixed determine how trending will show in query results
-    # normally the SQL will be: "ORDER BY trending_group, trending_mixed"
-    # changing the trending_group will have significant impact on trending results
-    # changing the value used for trending_mixed will only impact trending within a trending_group
-    db.execute(f"""
-    UPDATE claim SET
-        trending_group = CASE 
-        WHEN trending_local > 0 AND trending_global > 0 THEN 4
-        WHEN trending_local <= 0 AND trending_global > 0 THEN 3
-        WHEN trending_local > 0 AND trending_global <= 0 THEN 2
-        WHEN trending_local <= 0 AND trending_global <= 0 THEN 1
-        END,
-        trending_mixed = CASE 
-        WHEN trending_local > 0 AND trending_global > 0 THEN trending_global
-        WHEN trending_local <= 0 AND trending_global > 0 THEN trending_local
-        WHEN trending_local > 0 AND trending_global <= 0 THEN trending_local
-        WHEN trending_local <= 0 AND trending_global <= 0 THEN trending_global
-        END
-    WHERE trending_local <> 0 OR trending_global <> 0
-    """)
