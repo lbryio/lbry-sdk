c044e1ea8c598b7587c3ab54a4ae1cc7ab254068
Lex Berezhny <lex@damoti.com>
2019-12-15 01:02:18 -0500

    further refactor and simplification

--- lbry/tests/unit/lbrynet_daemon/test_exchange_rate_manager.py
+++ lbry/tests/unit/lbrynet_daemon/test_exchange_rate_manager.py
@@ -1,206 +1,137 @@
-import unittest
-import logging
+import asyncio
 from decimal import Decimal
+from time import time
 from lbry.schema.claim import Claim
-from lbry.extras.daemon import exchange_rate_manager
+from lbry.extras.daemon.exchange_rate_manager import (
+    ExchangeRate, ExchangeRateManager, CurrencyConversionError,
+    LBRYFeed, LBRYBTCFeed,
+    CryptonatorFeed, CryptonatorBTCFeed,
+    BittrexFeed,
+)
 from torba.testcase import AsyncioTestCase
 from lbry.error import InvalidExchangeRateResponseError
-from tests import test_utils
 
 
-class BTCLBCFeed(exchange_rate_manager.MarketFeed):
-    def __init__(self):
-        super().__init__(
-            "BTCLBC",
-            "market name",
-            "derp.com",
-            None,
-            0.0
-        )
-
-
-class USDBTCFeed(exchange_rate_manager.MarketFeed):
-    def __init__(self):
-        super().__init__(
-            "USDBTC",
-            "market name",
-            "derp.com",
-            None,
-            0.0
-        )
-
-
-class DummyExchangeRateManager(exchange_rate_manager.ExchangeRateManager):
+class DummyExchangeRateManager(ExchangeRateManager):
     def __init__(self, market_feeds, rates):
         self.market_feeds = market_feeds
         for feed in self.market_feeds:
-            feed.rate = exchange_rate_manager.ExchangeRate(
-                feed.market, rates[feed.market]['spot'], rates[feed.market]['ts'])
+            feed.last_check = time()
+            feed.rate = ExchangeRate(feed.market, rates[feed.market], time())
 
 
-def get_dummy_exchange_rate_manager(time):
-    rates = {
-        'BTCLBC': {'spot': 3.0, 'ts': time.time() + 1},
-        'USDBTC': {'spot': 2.0, 'ts': time.time() + 2}
-    }
-    return DummyExchangeRateManager([BTCLBCFeed(), USDBTCFeed()], rates)
+def get_dummy_exchange_rate_manager():
+    return DummyExchangeRateManager(
+        [LBRYFeed(), LBRYBTCFeed()],
+        {'BTCLBC': 3.0, 'USDBTC': 2.0}
+    )
 
 
-class ExchangeRateTest(unittest.TestCase):
-    def setUp(self):
-        test_utils.reset_time(self)
+class ExchangeRateTests(AsyncioTestCase):
 
     def test_invalid_rates(self):
         with self.assertRaises(ValueError):
-            exchange_rate_manager.ExchangeRate('USDBTC', 0, test_utils.DEFAULT_ISO_TIME)
+            ExchangeRate('USDBTC', 0, time())
         with self.assertRaises(ValueError):
-            exchange_rate_manager.ExchangeRate('USDBTC', -1, test_utils.DEFAULT_ISO_TIME)
-
-
-class FeeTest(unittest.TestCase):
-    def setUp(self):
-        test_utils.reset_time(self)
+            ExchangeRate('USDBTC', -1, time())
 
     def test_fee_converts_to_lbc(self):
         fee = Claim().stream.fee
         fee.usd = Decimal(10.0)
         fee.address = "bRcHraa8bYJZL7vkh5sNmGwPDERFUjGPP9"
-
-        rates = {
-            'BTCLBC': {'spot': 3.0, 'ts': test_utils.DEFAULT_ISO_TIME + 1},
-            'USDBTC': {'spot': 2.0, 'ts': test_utils.DEFAULT_ISO_TIME + 2}
-        }
-
-        market_feeds = [BTCLBCFeed(), USDBTCFeed()]
-        manager = DummyExchangeRateManager(market_feeds, rates)
+        manager = get_dummy_exchange_rate_manager()
         result = manager.convert_currency(fee.currency, "LBC", fee.amount)
         self.assertEqual(60.0, result)
 
     def test_missing_feed(self):
-        # test when a feed is missing for conversion
         fee = Claim().stream.fee
         fee.usd = Decimal(1.0)
         fee.address = "bRcHraa8bYJZL7vkh5sNmGwPDERFUjGPP9"
-
-        rates = {
-            'BTCLBC': {'spot': 1.0, 'ts': test_utils.DEFAULT_ISO_TIME + 1},
-        }
-        market_feeds = [BTCLBCFeed()]
-        manager = DummyExchangeRateManager(market_feeds, rates)
-        with self.assertRaises(Exception):
+        manager = DummyExchangeRateManager([LBRYFeed()], {'BTCLBC': 1.0})
+        with self.assertRaises(CurrencyConversionError):
             manager.convert_currency(fee.currency, "LBC", fee.amount)
 
-
-class LBRYioFeedTest(unittest.TestCase):
-    def test_handle_response(self):
-        feed = exchange_rate_manager.LBRYioFeed()
-
-        response = {
+    def test_lbry_feed_response(self):
+        feed = LBRYFeed()
+        out = feed.get_rate_from_response({
             'data': {
-                'fresh': 0, 'lbc_usd': 0.05863062523378918, 'lbc_btc': 5.065289549855739e-05, 'btc_usd': 1157.498
+                'fresh': 0, 'lbc_usd': 0.05863062523378918,
+                'lbc_btc': 5.065289549855739e-05, 'btc_usd': 1157.498
             },
             'success': True,
             'error': None
-        }
-        out = feed._handle_response(response)
-        expected = 1.0 / 5.065289549855739e-05
-        self.assertEqual(expected, out)
-
-        response = {}
+        })
+        self.assertEqual(1.0 / 5.065289549855739e-05, out)
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
-
-        response = {
-            "success": True,
-            "result": []
-        }
+            feed.get_rate_from_response({})
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
+            feed.get_rate_from_response({
+                "success": True,
+                "result": []
+            })
 
-
-class TestExchangeRateFeeds(unittest.TestCase):
-    def test_handle_lbryio_btc_response(self):
-        feed = exchange_rate_manager.LBRYioBTCFeed()
-
-        response = {
+    def test_lbry_btc_feed_response(self):
+        feed = LBRYBTCFeed()
+        out = feed.get_rate_from_response({
             'data': {
-                'fresh': 0, 'lbc_usd': 0.05863062523378918, 'lbc_btc': 5.065289549855739e-05, 'btc_usd': 1157.498
+                'fresh': 0, 'lbc_usd': 0.05863062523378918,
+                'lbc_btc': 5.065289549855739e-05, 'btc_usd': 1157.498
             },
             'success': True,
             'error': None
-        }
-        out = feed._handle_response(response)
-        expected = 1.0 / 1157.498
-        self.assertEqual(expected, out)
-
-        response = {}
+        })
+        self.assertEqual(1.0 / 1157.498, out)
         with self.assertRaises(InvalidExchangeRateResponseError):
-            out = feed._handle_response(response)
-
-        response = {
-            "success": True,
-            "result": {}
-        }
+            feed.get_rate_from_response({})
         with self.assertRaises(InvalidExchangeRateResponseError):
-            out = feed._handle_response(response)
+            feed.get_rate_from_response({
+                "success": True,
+                "result": {}
+            })
 
-    def test_handle_cryptonator_lbc_response(self):
-        feed = exchange_rate_manager.CryptonatorFeed()
-
-        response = {
+    def test_cryptonator_lbc_feed_response(self):
+        feed = CryptonatorFeed()
+        out = feed.get_rate_from_response({
             'ticker': {
-                'base': 'BTC', 'target': 'LBC', 'price': 23657.44026496, 'volume': '', 'change': -5.59806916,
+                'base': 'BTC', 'target': 'LBC', 'price': 23657.44026496,
+                'volume': '', 'change': -5.59806916,
             },
             'timestamp': 1507470422,
             'success': True,
             'error': ""
-        }
-        out = feed._handle_response(response)
-        expected = 23657.44026496
-        self.assertEqual(expected, out)
-
-        response = {}
+        })
+        self.assertEqual(23_657.44026496, out)
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
-
-        response = {
-            "success": True,
-            "ticker": {}
-        }
+            feed.get_rate_from_response({})
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
+            feed.get_rate_from_response({
+                "success": True,
+                "ticker": {}
+            })
 
-    def test_handle_cryptonator_btc_response(self):
-        feed = exchange_rate_manager.CryptonatorBTCFeed()
-
-        response = {
+    def test_cryptonator_btc_feed_response(self):
+        feed = CryptonatorBTCFeed()
+        out = feed.get_rate_from_response({
             'ticker': {
-                'base': 'BTC', 'target': 'LBC', 'price': 0.00022123, 'volume': '', 'change': -0.00000259,
+                'base': 'BTC', 'target': 'LBC', 'price': 0.00022123,
+                'volume': '', 'change': -0.00000259,
             },
             'timestamp': 1507471141,
             'success': True,
             'error': ''
-        }
-
-        out = feed._handle_response(response)
-        expected = 0.00022123
-        self.assertEqual(expected, out)
-
-        response = '{}'
+        })
+        self.assertEqual(0.00022123, out)
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
-
-        response = {
-            "success": True,
-            "ticker": {}
-        }
+            feed.get_rate_from_response({})
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
+            feed.get_rate_from_response({
+                "success": True,
+                "ticker": {}
+            })
 
-    def test_handle_bittrex_response(self):
-        feed = exchange_rate_manager.BittrexFeed()
-
-        response = {
+    def test_bittrex_feed_response(self):
+        feed = BittrexFeed()
+        out = feed.get_rate_from_response({
             "success": True,
             "message": "",
             "result": [
@@ -213,102 +144,27 @@ class TestExchangeRateFeeds(unittest.TestCase):
                     "Price": 0.00001592, "Total": 0.00686151, "FillType": "PARTIAL_FILL", "OrderType": "SELL"
                 }
             ]
-        }
-        out = feed._handle_response(response)
-        expected = 1.0 / ((0.00090980+0.00686151) / (56.12611239+430.99988180))
-        self.assertEqual(expected, out)
-
-        response = {}
+        })
+        self.assertEqual(1.0 / ((0.00090980+0.00686151) / (56.12611239+430.99988180)), out)
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
-
-        response = {
-            "success": True,
-            "result": []
-        }
+            feed.get_rate_from_response({})
         with self.assertRaises(InvalidExchangeRateResponseError):
-            feed._handle_response(response)
+            feed.get_rate_from_response({
+                "success": True,
+                "result": []
+            })
 
 
-class TestMarketFeed(AsyncioTestCase):
-    def setUp(self):
-        self.feed = exchange_rate_manager.MarketFeed('some market', 'some name', 'some url', {'param': 1}, 0.005)
+class BadMarketFeed(LBRYFeed):
 
-    def test_save_price(self):
-        with self.assertLogs('lbry.extras.daemon.exchange_rate_manager', logging.DEBUG) as cm:
-            self.feed._save_price(1)
-        self.assertIn(self.feed.market, ' '.join(cm.output))
-        self.assertIn(self.feed.name, ' '.join(cm.output))
-        self.assertTrue(self.feed.is_online())
-        self.assertIsNotNone(self.feed.rate)
-
-        with self.assertRaises(ValueError):
-            self.feed._save_price(0)
-
-        with self.assertRaises(TypeError):
-            self.feed._save_price('not a price')
-
-    async def test_update_price(self):
-        def mock_handle_response(json_obj):
-            return json_obj['data']['lbc_btc']
-
-        async def get_response_body_mock(self):
-            return '{\"data\": {\"fresh\": 0, \"lbc_usd\": 0.05863062523378918, ' \
-                   '\"lbc_btc\": 5.065289549855739e-05, \"btc_usd\": 1157.498}, ' \
-                   '\"success\": true, \"error\": null}'
-
-        self.feed._handle_response = mock_handle_response
-
-        with unittest.mock.patch.object(
-                exchange_rate_manager.AioHttpManager, 'get_response_body', get_response_body_mock
-        ):
-            await self.feed._update_price()
-        self.assertEqual(self.feed.rate.spot, 5.090743266186672e-05)
-        self.assertTrue(self.feed.is_online())
-
-        async def get_response_body_mock(self):
-            return '<h1>not a json</h1>'
-
-        with unittest.mock.patch.object(
-            exchange_rate_manager.AioHttpManager, 'get_response_body', get_response_body_mock
-        ), self.assertRaises(ValueError):
-            await self.feed._update_price()
+    def get_response(self):
+        raise InvalidExchangeRateResponseError(self.name, 'bad stuff')
 
 
-class TestDeserializer(unittest.TestCase):
-    def test_valid_json(self):
-        deserializer = exchange_rate_manager.Deserializer('json')
-        body = '{"data": "valid json", "some_float": 3.1415, "and_a_dict": {"value": true}}'
-        json_obj = deserializer.deserialize(body)
-        self.assertEqual(json_obj['data'], 'valid json')
-        self.assertEqual(json_obj['some_float'], 3.1415)
-        self.assertTrue(json_obj['and_a_dict']['value'])
+class ExchangeRateManagerTests(AsyncioTestCase):
 
-    def test_invalid_json(self):
-        def assert_raises_error(body):
-            with self.assertRaises(ValueError):
-                deserializer.deserialize(body)
-
-        deserializer = exchange_rate_manager.Deserializer('json')
-        assert_raises_error('<h1>not a json</h1>')
-        assert_raises_error('')
-        assert_raises_error('{')
-
-    def test_invalid_content_type(self):
-        with self.assertRaises(ValueError):
-            exchange_rate_manager.Deserializer('not a format')
-
-
-class TestAioHttpManager(AsyncioTestCase):
-    async def test_get_response_body(self):
-        async def make_request_mock(self):
-            response = unittest.mock.Mock(
-                headers={'Content-Type': 'jibberish'}
-            )
-            return response
-
-        manager = exchange_rate_manager.AioHttpManager('some url', 'some params', 'json')
-        with unittest.mock.patch.object(
-                exchange_rate_manager.AioHttpManager, '_make_request', make_request_mock
-        ), self.assertRaises(InvalidExchangeRateResponseError):
-            await manager.get_response_body()
+    async def test_get_rate_failure_retrieved(self):
+        manager = ExchangeRateManager([BadMarketFeed])
+        manager.start()
+        await asyncio.sleep(1)
+        self.addCleanup(manager.stop)
