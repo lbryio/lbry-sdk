867478697d084697dd1553458082958cf283468a
Victor Shyba <victor1984@riseup.net>
2020-01-03 02:57:36 -0300

    lint blob/*

--- lbry/blob/__init__.py
+++ lbry/blob/__init__.py
@@ -3,4 +3,4 @@ from lbry.utils import get_lbry_hash_obj
 MAX_BLOB_SIZE = 2 * 2 ** 20
 
 # digest_size is in bytes, and blob hashes are hex encoded
-blobhash_length = get_lbry_hash_obj().digest_size * 2
+BLOBHASH_LENGTH = get_lbry_hash_obj().digest_size * 2
