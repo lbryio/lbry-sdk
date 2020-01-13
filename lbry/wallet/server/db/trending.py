322182abfd4623d2df3dff671540fa4f7fe05bdf
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-01-14 11:27:00 +1300

    Enhanced minnow boost while trying not to encourage whales to script the same effect

--- lbry/wallet/server/db/trending.py
+++ lbry/wallet/server/db/trending.py
@@ -34,7 +34,7 @@ def spike_height(trending_score, x, x_old, time_boost=1.0):
     # Minnow boost
     boost = 0.0
     if spike_height > 0.0 and (trending_score + spike_height) > 0.0:
-        boost = math.exp(-(trending_score + spike_height)/time_boost)
+        boost = math.exp(-0.5*(trending_score + spike_height)/time_boost)
     spike_height += time_boost*boost
 
     return spike_height
