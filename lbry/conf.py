506d7ae50ea366f5823adaed4332019b0d552079
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2020-03-23 10:20:13 -0400

    default to 1080P for anything higher than 1080P
    Previously would do 1440p

--- lbry/conf.py
+++ lbry/conf.py
@@ -473,7 +473,7 @@ class TranscodeConfig(BaseConfig):
     video_bitrate_maximum = Integer('Maximum bits per second allowed for video streams (0 to disable).', 8400000)
     video_scaler = String('FFmpeg scaling parameters for reducing bitrate. '
                           'Example: -vf "scale=-2:720,fps=24" -maxrate 5M -bufsize 3M',
-                          r'-vf "scale=if(gte(iw\,ih)\,min(2560\,iw)\,-2):if(lt(iw\,ih)\,min(2560\,ih)\,-2)" '
+                          r'-vf "scale=if(gte(iw\,ih)\,min(1920\,iw)\,-2):if(lt(iw\,ih)\,min(1920\,ih)\,-2)" '
                           r'-maxrate 8400K -bufsize 5000K')
     audio_encoder = String('FFmpeg codec and parameters for the audio encoding. '
                            'Example: libopus -b:a 128k',
