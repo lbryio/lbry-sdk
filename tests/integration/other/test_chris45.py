c291f1179fa6365c3d0104fbbadbcb8e03720505
Jack Robison <jackrobison@lbry.io>
2021-06-06 13:08:15 -0400

    remove extra open functions

--- tests/integration/other/test_chris45.py
+++ tests/integration/other/test_chris45.py
@@ -199,5 +199,5 @@ class EpicAdventuresOfChris45(CommandTestCase):
         # He closes and opens the wallet server databases to see how horribly they break
         db = self.conductor.spv_node.server.db
         db.close()
-        await db.open_for_serving()
+        await db.open_dbs()
         # They didn't! (error would be AssertionError: 276 vs 266 (264 counts) on startup)
