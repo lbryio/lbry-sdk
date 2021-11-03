acda1074bf0188032a28a40a4bd4dd8f5f86728b
Victor Shyba <victor.shyba@gmail.com>
2021-11-02 22:34:15 -0300

    improve disk space manager status, include more info and unify space queries

--- lbry/extras/daemon/storage.py
+++ lbry/extras/daemon/storage.py
@@ -458,24 +458,34 @@ class SQLiteStorage(SQLiteMixin):
             "where blob.is_mine=? order by blob.added_on asc",
             (is_mine,)
         )
-        stream_blobs = await self.db.execute_fetchall(
+        content_blobs = await self.db.execute_fetchall(
             "select blob.blob_hash, blob.blob_length, blob.added_on "
             "from blob join stream_blob using (blob_hash) cross join stream using (stream_hash)"
             "cross join file using (stream_hash) where blob.is_mine=? order by blob.added_on asc",
             (is_mine,)
         )
-        return stream_blobs + sd_blobs
+        return content_blobs + sd_blobs
 
-    async def get_stored_blob_disk_usage(self, is_mine: Optional[bool] = None, is_network_blob: bool = False):
-        sql = "select coalesce(sum(blob_length), 0) "
-        if is_network_blob:
-            sql += "from blob left join stream_blob using (blob_hash) where stream_blob.stream_hash is null"
-        else:
-            sql += "from blob join stream_blob using (blob_hash)"
-        if is_mine is not None:
-            sql += f'{(" and " if is_network_blob else " where ")} is_mine=?'
-        args = (1 if is_mine else 0,) if is_mine is not None else ()
-        return (await self.db.execute_fetchone(sql, args))[0]
+    async def get_stored_blob_disk_usage(self):
+        total, network_size, content_size, private_size = await self.db.execute_fetchone("""
+        select coalesce(sum(blob_length), 0) as total,
+               coalesce(sum(case when
+                   stream_blob.stream_hash is null
+               then blob_length else 0 end), 0) as network_storage,
+               coalesce(sum(case when
+                   stream_blob.blob_hash is not null and is_mine=0
+               then blob_length else 0 end), 0) as content_storage,
+               coalesce(sum(case when
+                   is_mine=1
+               then blob_length else 0 end), 0) as private_storage
+        from blob left join stream_blob using (blob_hash)
+        """)
+        return {
+            'network_storage': network_size,
+            'content_storage': content_size,
+            'private_storage': private_size,
+            'total': total
+        }
 
     async def update_blob_ownership(self, sd_hash, is_mine: bool):
         is_mine = 1 if is_mine else 0
