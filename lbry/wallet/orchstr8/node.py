3436965b339528d01b22dd73180fa252bc86d4a9
Jeffrey Picard <jeff@jeffreypicard.com>
2021-12-03 15:51:54 -0500

    Debugging

--- lbry/wallet/orchstr8/node.py
+++ lbry/wallet/orchstr8/node.py
@@ -473,6 +473,10 @@ class HubProcess(asyncio.SubprocessProtocol):
             raise SystemError(data.decode())
         if b'listening on' in data:
             self.ready.set()
+        str_lines = str(data.decode()).split("\n")
+        for line in str_lines:
+            if 'releaseTime' in line:
+                print(line)
 
     def process_exited(self):
         self.stopped.set()
