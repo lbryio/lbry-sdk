44604e471566a97c0d99bf6cfc62550f086caaac
Victor Shyba <victor.shyba@gmail.com>
2021-12-20 11:21:20 -0300

    bump hub version to latest supporting sd_hash search

--- lbry/wallet/orchstr8/__init__.py
+++ lbry/wallet/orchstr8/__init__.py
@@ -1,5 +1,5 @@
 __hub_url__ = (
-    "https://github.com/lbryio/hub/releases/download/range-queries/hub"
+    "https://github.com/lbryio/hub/releases/download/v0.2021.12.18.1/hub"
 )
 from .node import Conductor
 from .service import ConductorService
