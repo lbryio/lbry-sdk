378a6471efa71ec1b7605a68fbdbfd56a51151f7
Jeffrey Picard <jeff@jeffreypicard.com>
2022-01-21 12:27:58 -0500

    Use latest go hub binary

--- lbry/wallet/orchstr8/__init__.py
+++ lbry/wallet/orchstr8/__init__.py
@@ -1,5 +1,5 @@
 __hub_url__ = (
-    "https://github.com/lbryio/hub/releases/download/v0.2021.12.18.1/hub"
+    "https://github.com/lbryio/hub/releases/download/v0.2022.01.21.1/hub"
 )
 from .node import Conductor
 from .service import ConductorService
