0e6079292e2bffb362efd41c340a0264a5795252
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 02:00:24 +0000

    Adding tarfile member sanitization to extractall()

--- lbry/wallet/orchstr8/node.py
+++ lbry/wallet/orchstr8/node.py
@@ -408,7 +408,26 @@ class LBCDNode:
 
         elif downloaded_file.endswith('.tar.gz'):
             with tarfile.open(downloaded_file) as tar:
-                tar.extractall(self.bin_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.bin_dir)
 
         return self.exists
 
@@ -540,7 +559,26 @@ class LBCWalletNode:
 
         elif downloaded_file.endswith('.tar.gz'):
             with tarfile.open(downloaded_file) as tar:
-                tar.extractall(self.bin_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.bin_dir)
 
         return self.exists
 
